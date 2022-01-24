d96cad09f2608c9e917cfa5037b4af9a12dcfba1
Akshay Gupta <kitallis@gmail.com>
2022-01-24 20:49:56 +0530

    Remove dead code

--- app/models/payments/concerns/subscribeable.rb
+++
@@ -1,4 +0,0 @@
-module Payments::Subscribeable
-  extend ActiveSupport::Concern
-  # This currently does nothing and exists only to serve as a template for overall modelling.
-end
--- app/models/payments/processors/stripe.rb
+++
@@ -1,3 +0,0 @@
-class Payments::Processors::Stripe
-  # This currently does nothing and exists only to serve as a template for overall modelling.
-end
