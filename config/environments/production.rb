485ce16b540dbb7b74aedb016aa3c05184f19d76
Akshay Gupta <kitallis@gmail.com>
2022-01-24 21:50:07 +0530

    Updating some configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,9 +64,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :postmark
-  config.action_mailer.postmark_settings = { :api_token => ENV["POSTMARK-SERVER-API-TOKEN"] }
-  config.action_mailer.default_url_options = { :host => ENV["MAILER-HOST"] }
-  Rails.application.routes.default_url_options[:host] = ENV["MAILER-HOST"]
+  config.action_mailer.postmark_settings = { :api_token => ENV["POSTMARK_SERVER_API_TOKEN"] }
+  config.action_mailer.default_url_options = { :host => ENV["MAILER_HOST"] }
+  Rails.application.routes.default_url_options[:host] = ENV["MAILER_HOST"]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
