d3750533107c6128068f5f0ce2e066ff2e5a378d
Akshay Gupta <kitallis@gmail.com>
2022-02-03 22:38:34 +0530

    Reorder the stimulus controller load paths

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,16 +4,12 @@ import { application } from 'controllers/application'
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from '@hotwired/stimulus-loading'
-
-// Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
-// import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
-// lazyLoadControllersFrom("controllers", application)
+eagerLoadControllersFrom('controllers', application)
 
 // import Flatpickr
 import Flatpickr from 'stimulus-flatpickr'
+application.register('flatpickr', Flatpickr)
 
 // import InputSelect
 import InputSelect from './input_select'
-eagerLoadControllersFrom('controllers', application)
-application.register('flatpickr', Flatpickr)
 application.register('inputselect', InputSelect)
