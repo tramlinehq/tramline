c78207d247cf4162a69ca6f115c624f10c59799e
Akshay Gupta <kitallis@gmail.com>
2022-02-03 21:53:24 +0530

    Add a sidekiq server config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,10 +2,13 @@ module SidekiqConfig
   DEFAULT_SIDEKIQ_REDIS_POOL = 12
 
   def self.connection_pool
+    puts "hi2u"
     ConnectionPool.new(size: DEFAULT_SIDEKIQ_REDIS_POOL) do
       if ENV["SIDEKIQ_REDIS_URL"].present?
+        puts "hi2u"
         Redis.new(url: ENV["SIDEKIQ_REDIS_URL"])
       else
+        puts "hi2u"
         Redis.new
       end
     end
@@ -13,5 +16,10 @@ module SidekiqConfig
 end
 
 Sidekiq.configure_client do |config|
+  config.redis = SidekiqConfig.connection_pool
+end
+
+Sidekiq.configure_server do |config|
+  config.on(:shutdown) { Statsd.instance.close }
   config.redis = SidekiqConfig.connection_pool
 end
