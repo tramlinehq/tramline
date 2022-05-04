461fcdab2e7a6ac34425d41870c96a72e619c49f
Akshay Gupta <kitallis@gmail.com>
2022-05-05 00:02:23 +0530

    Remove some dead code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,4 @@
 module ApplicationHelper
-  def developer_commentary
-    return unless Flipper.enabled?(:developer_commentary)
-
-    content_tag(:dev, class: "play_developer-commentary") do
-      concat(content_tag(:strong, "Developer Commentary"))
-      yield
-    end
-  end
-
   def sidebar_active_link(path, style)
     if current_page?(path)
       style
