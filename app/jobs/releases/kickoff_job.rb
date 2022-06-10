17d757b1c4ef37f93480f8c85ed3833640f35259
Aboobacker MK <aboobackervyd@gmail.com>
2022-06-10 16:37:35 +0530

    Remove rest of traces

--- app/jobs/releases/kickoff_job.rb
+++ app/jobs/releases/kickoff_job.rb
@@ -5,9 +5,7 @@ class Releases::KickoffJob
 
   def perform(*args)
     Releases::Train.active.each do |train|
-      if train.runnable?
-        TrainJob.perform_now(train.id)
-      end
+      TrainJob.perform_now(train.id)
     end
   end
 end
