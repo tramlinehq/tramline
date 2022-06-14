77e42f90cc51ea4f22c3d3d07d086ae56d17af30
Aboobacker MK <aboobackervyd@gmail.com>
2022-06-14 19:57:39 +0530

    Remove explict require of choices js

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -1,13 +1,18 @@
 import { Controller } from "stimulus"
+import Choices from 'choices.js'
 
 export default class extends Controller {
   static targets = ["add_item", "template"]
+
+  initialize() {
+    this.reset_choices()
+  }
    
   add_association(event) {
     event.preventDefault()  
     var content = this.templateTarget.innerHTML.replace(/TEMPLATE_RECORD/g, new Date().valueOf())
     this.add_itemTarget.insertAdjacentHTML('beforebegin', content)
-    let elements = [...document.querySelectorAll('.js-choice')].map(el => new Choices(el))
+    this.reset_choices()
   }
 
   remove_association(event) {
@@ -16,4 +21,8 @@ export default class extends Controller {
     item.querySelector("input[name*='_destroy']").value = 1
     item.style.display = 'none'
   }
+
+  reset_choices() {
+    let elements = [...document.querySelectorAll('.js-choice')].map(el => new Choices(el, { allowHTML: false }))
+	}
 }
