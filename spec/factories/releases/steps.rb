ddbf91ce6213c9999b7b66ad6201ec3c658a5223
Aboobacker MK <aboobackervyd@gmail.com>
2022-06-16 13:22:24 +0530

    Remove removed attribute

--- spec/factories/releases/steps.rb
+++ spec/factories/releases/steps.rb
@@ -3,7 +3,6 @@ FactoryBot.define do
     association :train, factory: :releases_train
     name { Faker::Lorem.word }
     description { Faker::Lorem.sentence }
-    run_after_duration { Faker::Number.number(digits: 2) }
     ci_cd_channel { Faker::Lorem.word }
     build_artifact_channel { Faker::Lorem.word }
   end
