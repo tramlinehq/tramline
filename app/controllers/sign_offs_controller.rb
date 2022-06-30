740612292eb40dfa8d2fbc1d9c63d5b1a4b4522a
Aboobacker MK <aboobackervyd@gmail.com>
2022-06-30 14:53:11 +0530

    Add authorisation to releases and sign_off_grouoe end points

--- app/controllers/sign_offs_controller.rb
+++ app/controllers/sign_offs_controller.rb
@@ -1,6 +1,7 @@
-class SignOffsController < SignedInApplicationController
+class Accounts::Releases::SignOffsController < SignedInApplicationController
+  before_action :set_step, only: [:create, :destroy]
+
   def create
-    @step = Releases::Step.friendly.find(params[:step_id])
     @sign_off_group = SignOffGroup.find(params[:sign_off_group_id])
     if @sign_off_group.members.include?(current_user)
       @step_sign_off = @step.sign_offs.create!(user_id: current_user.id, sign_off_group: @sign_off_group, signed: true)
@@ -11,9 +12,14 @@ class SignOffsController < SignedInApplicationController
   end
 
   def destroy
-    @step = Releases::Step.friendly.find(params[:step_id])
     @sign_off_group = SignOffGroup.find(params[:sign_off_group_id])
     @step.sign_offs.where(sign_off_group: @sign_off_group).update(signed: false)
     redirect_back fallback_location: root_path, notice: "You have unsigned this step"
+  end
+
+  private
+
+  def set_step
+    @step = Releases::Step.joins(train: :app).where(trains: {apps: {organization: current_organization}}).friendly.find(params[:step_id])
   end
 end
