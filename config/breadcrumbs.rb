74df61d6997b26b4fb5ad9dc01c64629d23691fb
Aboobacker MK <aboobackervyd@gmail.com>
2022-07-14 11:34:04 +0530

    Improve breadcrumb navidation

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,10 +1,10 @@
 crumb :root do
-  link 'Home', root_path
+  link "Home", root_path
 end
 
 # Issue list
 crumb :apps do
-  link 'All apps', apps_path
+  link "All apps", apps_path
 end
 
 crumb :app do |app|
@@ -28,41 +28,26 @@ crumb :release do |release|
 end
 
 crumb :app_config do |config|
-  link 'App config', edit_app_app_config_path(config.app, config)
+  link "App config", edit_app_app_config_path(config.app, config)
   parent :app, config.app
 end
 
 crumb :integrations do |app|
-  link 'integrations', app_integrations_path(app)
+  link "integrations", app_integrations_path(app)
   parent :app, app
 end
 
 crumb :sign_off_groups do |app|
-  link 'Sign Off Config', app_sign_off_groups_path(app)
+  link "Sign Off Config", app_sign_off_groups_path(app)
   parent :app, app
 end
 
-# crumb :projects do
-#   link "Projects", projects_path
-# end
+crumb :new_train do |app|
+  link "New Train"
+  parent :app, app
+end
 
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
-
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
-
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
-
-# If you want to split your breadcrumbs configuration over multiple files, you
-# can create a folder named `config/breadcrumbs` and put your configuration
-# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
-# folder are loaded and reloaded automatically when you change them, just like
-# this file (`config/breadcrumbs.rb`).
+crumb :edit_train do |train|
+  link "Edit"
+  parent :train, train
+end
