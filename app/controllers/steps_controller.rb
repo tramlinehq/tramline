4cac288889541b4e7cbd94a9b59f11613ada8736
Akshay Gupta <kitallis@gmail.com>
2022-08-16 15:49:50 +0530

    Set external build artifact configuration correctly (#139)
    This is a hacky quickfix, that puts an artificial json configuration under the step's build channel config specifically for external deployments. This really needs to be solved with a better abstraction.

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -31,18 +31,29 @@ class StepsController < SignedInApplicationController
   end
 
   def edit
-    @step = Releases::Step.joins(train: :app).where(trains: {apps: {organization: current_organization}}).friendly.find(params[:id])
+    @step =
+      Releases::Step
+        .joins(train: :app)
+        .where(trains: {apps: {organization: current_organization}})
+        .friendly
+        .find(params[:id])
     @train = @step.train
-
     head 403 and return if @train.active_run
+
     @build_channels = @step.available_deployment_channels
     @ci_actions = @train.ci_cd_provider.workflows
   end
 
   def update
-    @step = Releases::Step.joins(train: :app).where(trains: {apps: {organization: current_organization}}).friendly.find(params[:id])
+    @step =
+      Releases::Step
+        .joins(train: :app)
+        .where(trains: {apps: {organization: current_organization}})
+        .friendly
+        .find(params[:id])
     @train = @step.train
     head 403 and return if @train.active_run
+
     @app = @train.app
 
     if @step.update(parsed_step_params)
@@ -59,11 +70,12 @@ class StepsController < SignedInApplicationController
     train = Releases::Train.friendly.find(params[:train_id])
     provider = params[:provider]
 
-    @build_channels = if provider == "external" # TODO: Have a better abstraction instead of if conditions
-      {"external" => "external"}
-    else
-      train.app.integrations.build_channel.find_by(providable_type: provider).providable.channels
-    end
+    @build_channels =
+      if provider == "external" # TODO: Have a better abstraction instead of if conditions
+        [["External", {"external" => "external"}.to_json]]
+      else
+        train.app.integrations.build_channel.find_by(providable_type: provider).providable.channels
+      end
 
     respond_to do |format|
       format.turbo_stream
