f09128db3085ebc7e0e1f4acf1df8eb735307abe
Akshay Gupta <kitallis@gmail.com>
2022-08-19 14:07:36 +0530

    Only add a new blank sign-off group when none present

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -1,18 +1,10 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["addItem", "template", "form"]
-
-  initialize() {
-    this.insertContent()
-  }
+  static targets = ["addItem", "template"]
 
   addAssociation(e) {
     e.preventDefault()
-    this.insertContent()
-  }
-
-  insertContent() {
     const content = this.templateTarget.innerHTML.replace(/TEMPLATE_RECORD/g, new Date().valueOf())
     this.addItemTarget.insertAdjacentHTML("beforebegin", content)
   }
