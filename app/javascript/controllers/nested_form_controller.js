e4aa8c1009f3cf49412e135ea9b4d2ceed508b72
Akshay Gupta <kitallis@gmail.com>
2022-08-19 13:03:24 +0530

    Move sign-offs to slim-select + other minor ux enhancements

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -1,28 +1,11 @@
 import { Controller } from "stimulus"
-import Choices from 'choices.js'
 
 export default class extends Controller {
-  static targets = ["add_item", "template"]
+  static targets = ["addItem", "template"]
 
-  initialize() {
-    this.reset_choices()
-  }
-
-  add_association(event) {
+  addAssociation(event) {
     event.preventDefault()
     var content = this.templateTarget.innerHTML.replace(/TEMPLATE_RECORD/g, new Date().valueOf())
-    this.add_itemTarget.insertAdjacentHTML("beforebegin", content)
-    this.reset_choices()
+    this.addItemTarget.insertAdjacentHTML("beforebegin", content)
   }
-
-  remove_association(event) {
-    event.preventDefault()
-    let item = event.target.closest(".nested-fields")
-    item.querySelector("input[name*='_destroy']").value = 1
-    item.style.display = 'none'
-  }
-
-  reset_choices() {
-    let elements = [...document.querySelectorAll('.js-choice')].map(el => new Choices(el, { allowHTML: false }))
-	}
 }
