da6a97e68d4d8425c4c63303fa7fa1bea868df64
Akshay Gupta <kitallis@gmail.com>
2022-09-06 13:57:48 +0530

    Dead code: Remove email automaton

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,9 +1,6 @@
 class TestMailer < ApplicationMailer
   def verify
     @user = Accounts::User.find(params[:user_id])
-    @was_run_at = params[:was_run_at]
-    @train_name = params[:train_name]
-
-    mail(to: @user.email, subject: "Train started successfully!")
+    mail(to: @user.email, subject: "Test email from Tramline")
   end
 end
