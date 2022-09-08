e8cd334e094cf104162ea0274582a609e8d2f17f
Akshay Gupta <kitallis@gmail.com>
2022-09-08 19:11:41 +0530

    Better names in SiteExtensions

--- lib/site_extensions.rb
+++ lib/site_extensions.rb
@@ -2,17 +2,17 @@ module SiteExtensions
   require "ostruct"
 
   def self.determine_git_ref
-    OpenStruct.new(git_ref: `git rev-parse --short HEAD`.chomp, git_ref_at: `git show -s --format=%ci HEAD`.chomp)
+    OpenStruct.new(ref: `git rev-parse --short HEAD`.chomp, at: `git show -s --format=%ci HEAD`.chomp)
   end
 
   GIT_REF = determine_git_ref
 
   def git_ref
-    GIT_REF.git_ref
+    GIT_REF.ref
   end
 
   def git_ref_at
-    Time.parse GIT_REF.git_ref_at
+    Time.parse GIT_REF.at
   end
 end
 
