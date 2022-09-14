abc7be0cbcf14405d77a0093a1bcedc6d0f09a5d
Akshay Gupta <kitallis@gmail.com>
2022-09-14 12:58:33 +0530

    Make webhook processing async

--- app/libs/notifiers/slack/build_finished.rb
+++ app/libs/notifiers/slack/build_finished.rb
@@ -5,19 +5,19 @@ module Notifiers
       RUN_URI =
         Addressable::Template.new("https://api.github.com/repos/{org_name}/{repo_name}/actions/runs/{run_id}/artifacts")
 
-      def initialize(artifact_link:, code_name:, build_number:, version_number:, branch_name:)
+      def initialize(artifacts_url:, code_name:, build_number:, version_number:, branch_name:)
         @code_name = code_name
         @branch_name = branch_name
         @build_number = build_number
         @version_number = version_number
-        @artifact_link = artifact_link(artifact_link)
+        @artifacts_url = artifacts_url(artifacts_url)
         super
       end
 
       private
 
-      def artifact_link(link)
-        extracted = RUN_URI.extract(link)
+      def artifacts_url(url)
+        extracted = RUN_URI.extract(url)
         "https://github.com/#{extracted["org_name"]}/#{extracted["repo_name"]}/actions/runs/#{extracted["run_id"]}"
       end
 
--- app/libs/webhook_handlers/github/workflow_run.rb
+++ app/libs/webhook_handlers/github/workflow_run.rb
@@ -20,75 +20,40 @@ class WebhookHandlers::Github::WorkflowRun
     return Response.new(:unprocessable_entity) if train.blank?
     return Response.new(:unprocessable_entity) if train.inactive?
     return Response.new(:accepted) if release.blank?
-    return Response.new(:accepted) if step_run.blank?
 
-    if successful?
-      transaction do
-        add_step_run_metadata!
-        update_status!
-        upload_artifact!
-        notify!
-      end
-    else
-      update_status!
-    end
-
+    WebhookProcessors::Github::WorkflowRun.perform_later(release.id, workflow_attributes)
     Response.new(:accepted)
   end
 
   private
 
-  def update_status!
-    step_run.ci_failed! and return if failed?
-    step_run.about_to_deploy! and return if successful?
-    step_run.ci_cancelled! if halted?
+  def workflow_attributes
+    {
+      conclusion: conclusion,
+      artifacts_url: artifacts_url,
+      ci_ref: payload[:workflow_run][:id],
+      ci_link: payload[:workflow_run][:html_url]
+    }
   end
 
-  def add_step_run_metadata!
-    step_run.update!(ci_ref: payload[:workflow_run][:id], ci_link: payload[:workflow_run][:html_url])
-  end
-
-  def upload_artifact!
-    Releases::Step::UploadArtifact.perform_later(step_run.id, installation_id, artifacts_url)
-  end
-
-  def notify!
-    release_branch = release.release_branch
-    code_name = release.code_name
-    build_number = step_run.build_number
-    version_number = step_run.build_version
-
-    text_block =
-      Notifiers::Slack::BuildFinished.render_json(
-        artifact_link: artifacts_url,
-        code_name:,
-        branch_name: release_branch,
-        build_number:,
-        version_number:
-      )
-
-    Automatons::Notify.dispatch!(train:, message: "Your release workflow completed!", text_block:)
-  end
-
-  def step_run
-    @step_run ||=
-      begin
-        version_zip = installation.artifact_io_stream(version_artifact_url)
-        build_number = Zip::File.open(version_zip).entries.first.get_input_stream.read.strip
-        release.step_runs.ci_workflow_started.find_by(build_number: build_number)
-      end
-  end
-
-  def version_artifact_url
-    installation
-      .artifacts(artifacts_url)
-      .find { |artifact| artifact["name"] == "version" }["archive_download_url"]
+  def conclusion
+    return :success if successful?
+    return :failed if failed?
+    :halted if halted?
   end
 
   # TODO: These checks are a workaround because Github's action, status and conclusion fields can be pretty unreliable
   def successful?
     (complete_action? && payload_status == "in_progress") ||
       (payload_status == "completed" && payload_conclusion == "success")
+  end
+
+  def failed?
+    complete_action? && payload_conclusion == "failure"
+  end
+
+  def halted?
+    complete_action? && payload_status == "completed" && payload_conclusion == "cancelled"
   end
 
   def payload_status
@@ -109,25 +74,5 @@ class WebhookHandlers::Github::WorkflowRun
 
   def complete_action?
     payload_action == "completed"
-  end
-
-  def failed?
-    complete_action? && payload_conclusion == "failure"
-  end
-
-  def halted?
-    complete_action? && payload_status == "completed" && payload_conclusion == "cancelled"
-  end
-
-  def artifacts_name
-    installation.artifact_filename(artifacts_url)
-  end
-
-  def installation
-    @installation ||= Installations::Github::Api.new(installation_id)
-  end
-
-  def installation_id
-    @installation_id ||= train.ci_cd_provider.installation_id
   end
 end
