535b4015b4816ac4ee90e0bede2fefd8f7030097
Akshay Gupta <kitallis@gmail.com>
2022-09-22 13:45:04 +0530

    Webhook limit copy

--- app/models/releases/train.rb
+++ app/models/releases/train.rb
@@ -71,7 +71,7 @@ class Releases::Train < ApplicationRecord
     return true if integrations.shared_vcs_and_ci_cd?
     ci_cd_provider.create_webhook!(train_id: id)
   rescue Installations::Errors::WebhookLimitReached
-    errors.add(:webhooks, "The webhooks limit was hit. Please clear some integration webhooks before proceeding!")
+    errors.add(:webhooks, "We can't create any more webhooks in your VCS/CI environment!")
     raise ActiveRecord::RecordInvalid, self
   end
 
