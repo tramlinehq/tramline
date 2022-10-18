1ccf93bab345fb1ce26dbf2bb336cba599a5e6b2
Akshay Gupta <kitallis@gmail.com>
2022-10-18 17:01:58 +0530

    Show stale commits

--- app/models/releases/commit.rb
+++ app/models/releases/commit.rb
@@ -6,9 +6,13 @@ class Releases::Commit < ApplicationRecord
   has_many :step_runs, class_name: "Releases::Step::Run", dependent: :nullify, foreign_key: "releases_commit_id", inverse_of: :commit
   has_many :sign_offs, foreign_key: "releases_commit_id", dependent: :destroy, inverse_of: :commit
 
-  validates :commit_hash, uniqueness: {scope: :train_run_id}
+  validates :commit_hash, uniqueness: { scope: :train_run_id }
 
   def step_runs_for(step)
     step_runs.where(step: step)
+  end
+
+  def stale?
+    train_run.commits.last != self
   end
 end
