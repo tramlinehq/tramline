cb9a723f3177d9f3882c3d57916482e1d09d91f8
Akshay Gupta <kitallis@gmail.com>
2022-11-03 13:58:55 +0530

    Revert "Revert "Upgrade Sidekiq to 7 (#240)""
    This reverts commit 80b70d8b43457b625372b9386bbf1b7f41005a3e.

--- config/initializers/sidekiq.rb
+++
@@ -1,9 +0,0 @@
-require "sidekiq_config"
-
-Sidekiq.configure_client do |config|
-  config.redis = SidekiqConfig.connection_pool
-end
-
-Sidekiq.configure_server do |config|
-  config.redis = SidekiqConfig.connection_pool
-end
