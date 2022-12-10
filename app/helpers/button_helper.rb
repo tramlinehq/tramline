2b6c1943cfb32f80a5f05f07d5d250e5d90abc3c
Akshay Gupta <kitallis@gmail.com>
2022-12-10 14:37:02 +0530

    Auth-aware and decorated buttons+tags, links

--- app/helpers/button_helper.rb
+++ app/helpers/button_helper.rb
@@ -1,29 +1,64 @@
 module ButtonHelper
-  BUTTON_STYLES = {
-    green: "bg-emerald-500 hover:bg-emerald-600 text-white",
-    red: "",
-    neutral: "text-slate-600 border-slate-300 hover:border-slate-400",
-    disabled: "opacity-30 cursor-not-allowed border-slate-300 hover:border-slate-300"
+  BUTTON_OPTIONS = {
+    green: {
+      class: " btn bg-emerald-500 hover:bg-emerald-600 text-white"
+    },
+    blue: {
+      class: " btn bg-indigo-500 hover:bg-indigo-600 text-white"
+    },
+    red: {
+      class: " btn bg-rose-500 hover:bg-rose-600 text-white"
+    },
+    neutral: {
+      class: " btn text-slate-600 border-slate-300 hover:border-slate-400"
+    },
+    disabled:
+      {
+        class: " btn opacity-30 disabled cursor-not-allowed border-slate-300 hover:border-slate-300",
+        disabled: true
+      }
   }
 
-  def button_link_to(style, name = nil, options = nil, html_options = nil, &block)
-    styles = " btn " + BUTTON_STYLES[style]
+  def apply_options(options, new_options)
+    options ||= {}
+    options[:class] ||= " "
+    options[:class] << new_options[:class]
+    options.merge(new_options.except(:class))
+  end
+
+  def apply_button_styles(style, options, html_options, block)
+    new_opts = BUTTON_OPTIONS[style]
 
     if block
-      options ||= {class: ""}
-      options[:class] << styles
+      options = apply_options(options, new_opts)
     else
-      html_options ||= {class: ""}
-      html_options[:class] << styles
+      html_options = apply_options(html_options, new_opts)
     end
 
+    [options, html_options]
+  end
+
+  # link that looks like a styled button
+  def decorated_link_to(style, name = nil, options = nil, html_options = nil, &block)
+    options, html_options = apply_button_styles(style, options, html_options, block)
     link_to(name, options, html_options, &block)
   end
 
+  # styled button with path
+  def decorated_button_to(style, name = nil, options = nil, html_options = nil, &block)
+    options, html_options = apply_button_styles(style, options, html_options, block)
+    button_to(name, options, html_options, &block)
+  end
+
+  # styled button tag
+  def decorated_button_tag(style, options = nil, html_options = nil, &block)
+    options, html_options = apply_button_styles(style, options, html_options, block)
+    button_tag(options, html_options, &block)
+  end
+
+  # auth-aware link that looks like a styled button
   def authz_link_to(style, name = nil, options = nil, html_options = nil, &block)
-    unless writer?
-      style = :disabled
-    end
+    style = :disabled unless writer?
 
     if block
       unless writer?
@@ -35,9 +70,16 @@ module ButtonHelper
       end
     end
 
-    button_link_to(style, name, options, html_options, &block)
+    decorated_link_to(style, name, options, html_options, &block)
   end
 
+  # auth-aware styled button with path
+  def authz_button_to(style, name = nil, options = nil, html_options = nil, &block)
+    style = :disabled unless writer?
+    decorated_button_to(style, name, options, html_options, &block)
+  end
+
+  # open a link in a new tab
   def link_to_external(name = nil, options = nil, html_options = nil, &block)
     opts = {target: "_blank", rel: "nofollow noopener"}
 
@@ -50,5 +92,23 @@ module ButtonHelper
     end
 
     link_to(name, options, html_options, &block)
+  end
+
+  class AuthzForms < ActionView::Helpers::FormBuilder
+    def authz_submit(style, value = nil, options = nil)
+      new_opts =
+        if @template.writer?
+          BUTTON_OPTIONS[style]
+        else
+          BUTTON_OPTIONS[:disabled]
+        end
+
+      options ||= {}
+      options[:class] ||= " "
+      options[:class] << new_opts[:class]
+      options = options.merge(new_opts.except(:class))
+
+      submit(value, options)
+    end
   end
 end
