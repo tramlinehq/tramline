cead96aa9c2205267461dac093dc6fccb5ad0a91
Akshay Gupta <kitallis@gmail.com>
2023-01-09 16:41:55 +0530

    Revert "WIP"
    This reverts commit 7ce61977be238aa26bc73c6df90c2c6954f9b8c6.

--- app/models/deployment_run.rb
+++ app/models/deployment_run.rb
@@ -67,16 +67,18 @@ class DeploymentRun < ApplicationRecord
 
   def promote!
     save!
-    return unless promotable?
-    return unless deployment.integration.google_play_store_integration?
 
     release.with_lock do
+      return unless promotable?
+      return unless deployment.integration.google_play_store_integration?
+
       package_name = step.app.bundle_identifier
       release_version = step_run.train_run.release_version
       api = Installations::Google::PlayDeveloper::Api.new(package_name, deployment.access_key, release_version)
       api.promote(deployment.deployment_channel, step_run.build_number, initial_rollout_percentage)
 
       complete!
+
     rescue Installations::Errors::BuildNotUpgradable => e
       logger.error(e)
       Sentry.capture_exception(e)
