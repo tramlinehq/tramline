eaf74b687ba46825090d3278e423937217cf3f2e
Akshay Gupta <kitallis@gmail.com>
2023-01-18 19:40:12 +0530

    FIx test setup

--- spec/requests/accounts/releases/releases_spec.rb
+++ spec/requests/accounts/releases/releases_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "Accounts::Releases::Releases", type: :request do
   describe "GET /show" do
-    let(:release) { create(:releases_train_run) }
+    let(:release) { create(:releases_train_run, :created) }
     let(:organization) { release.train.app.organization }
     let(:user) { create(:user, :as_developer, confirmed_at: Time.zone.now, member_organization: organization) }
 
