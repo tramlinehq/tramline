ce0d8ea4deccc0922fbb5a292190d7f551cb6f78
Akshay Gupta <kitallis@gmail.com>
2023-01-18 10:14:53 +0530

    Simplify a few jobs before adding idempotency

--- app/jobs/deployments/slack.rb
+++ app/jobs/deployments/slack.rb
@@ -1,38 +1,9 @@
 class Deployments::Slack < ApplicationJob
   queue_as :high
-  delegate :transaction, to: DeploymentRun
-  MESSAGE = "A wild new release has appeared!"
 
   def perform(deployment_run_id)
-    @deployment_run = DeploymentRun.find(deployment_run_id)
-    return unless deployment.integration.slack_integration?
-
-    @deployment_run.with_lock do
-      # FIXME: this transaction can eventually be removed, just use Result objects
-      transaction do
-        push
-        @deployment_run.complete!
-      end
-    end
-  end
-
-  def push
-    provider.deploy!(channel, MESSAGE, :deployment_finished, {step_run: step_run})
-  end
-
-  def deployment
-    @deployment_run.deployment
-  end
-
-  def step_run
-    @deployment_run.step_run
-  end
-
-  def channel
-    deployment.deployment_channel
-  end
-
-  def provider
-    deployment.integration.providable
+    run = DeploymentRun.find(deployment_run_id)
+    return unless run.slack_integration?
+    run.with_lock { run.push_to_slack! }
   end
 end
