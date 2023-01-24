c18be14280950cea060c718c2e3a59c460ea258a
Akshay Gupta <kitallis@gmail.com>
2023-01-24 21:24:00 +0530

    Add spec for triggering step runs from commits

--- spec/models/releases/commit_spec.rb
+++ spec/models/releases/commit_spec.rb
@@ -4,4 +4,32 @@ describe Releases::Commit, type: :model do
   it "has valid factory" do
     expect(create(:releases_commit)).to be_valid
   end
+
+  describe "#trigger_step_runs" do
+    let(:active_train) { create(:releases_train, :active) }
+
+    context "triggers the step runs" do
+      it "does it for the first step run if first commit" do
+        train_run = create(:releases_train_run, train: active_train)
+        create(:releases_step, :with_deployment, train: active_train)
+
+        allow(Triggers::StepRun).to receive(:call)
+
+        create(:releases_commit, train: active_train, train_run: train_run)
+
+        expect(Triggers::StepRun).to have_received(:call)
+      end
+
+      it "does it for all steps until the currently running one" do
+        train_run = create(:releases_train_run, train: active_train)
+        create(:releases_step, :with_deployment, train: active_train)
+
+        allow(Triggers::StepRun).to receive(:call)
+
+        create(:releases_commit, train: active_train, train_run: train_run)
+
+        expect(Triggers::StepRun).to have_received(:call)
+      end
+    end
+  end
 end
