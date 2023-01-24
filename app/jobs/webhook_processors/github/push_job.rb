16310d755b1f7c5b9215728f9cf8187e62e909b6
Akshay Gupta <kitallis@gmail.com>
2023-01-24 18:09:58 +0530

    No need for lock in job

--- app/jobs/webhook_processors/github/push_job.rb
+++ app/jobs/webhook_processors/github/push_job.rb
@@ -2,10 +2,9 @@ class WebhookProcessors::Github::PushJob < ApplicationJob
   queue_as :high
 
   def perform(train_run_id, commit_attributes)
-    release = Releases::Train::Run.find(train_run_id)
-    release.with_lock do
-      return unless release.committable?
-      WebhookProcessors::Github::Push.process(release, commit_attributes)
-    end
+    run = Releases::Train::Run.find(train_run_id)
+    return unless run.committable?
+
+    WebhookProcessors::Github::Push.process(run, commit_attributes)
   end
 end
