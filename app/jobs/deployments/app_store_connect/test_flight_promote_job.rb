509746eab011d043297d3de12f97a95329b3f320
Nivedita <nid.mishra7@gmail.com>
2023-01-30 18:10:51 +0530

    Add specs for testflight promote job

--- app/jobs/deployments/app_store_connect/test_flight_promote_job.rb
+++ app/jobs/deployments/app_store_connect/test_flight_promote_job.rb
@@ -4,6 +4,8 @@ class Deployments::AppStoreConnect::TestFlightPromoteJob < ApplicationJob
   def perform(deployment_run_id)
     run = DeploymentRun.find(deployment_run_id)
     return unless run.app_store_integration?
+    return unless run.release.on_track?
+
     run.promote_to_appstore!
   end
 end
