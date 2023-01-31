a6ca4e80b15d7398f878e852e99525a85912babc
Akshay Gupta <kitallis@gmail.com>
2023-01-31 13:43:48 +0530

    Icon sprinklage

--- app/helpers/deployments_helper.rb
+++ app/helpers/deployments_helper.rb
@@ -10,4 +10,12 @@ module DeploymentsHelper
   def show_deployment_channel(deployment)
     deployment.build_artifact_channel["name"]
   end
+
+  def deployment_integration_name(deployment)
+    if deployment.external?
+      "external"
+    else
+      deployment.integration.providable.to_s
+    end
+  end
 end
