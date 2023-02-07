befade3b5ae45838233c685261e1b438a232314e
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-02-07 23:32:52 +0530

    Extract view logic in component

--- app/components/external_app_component.rb
+++ app/components/external_app_component.rb
@@ -10,4 +10,16 @@ class ExternalAppComponent < ViewComponent::Base
   def channels
     external_app.channel_data.map { |ch| ch.with_indifferent_access }
   end
+
+  def releases(channel)
+    channel[:releases].sort_by { |r| r[:status] }
+  end
+
+  def release_status(release)
+    release[:status].titleize.humanize
+  end
+
+  def release_percent(release)
+    release[:user_fraction] * 100
+  end
 end
