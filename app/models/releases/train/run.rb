1bfe9d54bbeb99b7a421bc1f251ba8511cc3e796
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-02-07 23:26:34 +0530

    Refresh external app details after train run finishes

--- app/models/releases/train/run.rb
+++ app/models/releases/train/run.rb
@@ -213,6 +213,7 @@ class Releases::Train::Run < ApplicationRecord
   def on_finish!
     event_stamp!(reason: :finished, kind: :success, data: {version: release_version})
     train.notify!("Release is complete!", :release_ended, finalize_phase_metadata)
+    app.refresh_external_app
   end
 
   def finalize_phase_metadata
