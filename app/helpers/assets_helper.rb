ece6dc41f120bfd7fe505a35a58de7ccc874dcb7
Akshay Gupta <kitallis@gmail.com>
2023-02-08 15:30:13 +0530

    Sanitizer for SVGs

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -1,22 +1,18 @@
 module AssetsHelper
-  def inline_file(asset_name)
-    if (asset = Rails.application.assets&.find_asset(asset_name))
-      sanitize asset.source
-    else
-      asset_path = Rails.application.assets_manifest.assets[asset_name]
-      sanitize Rails.root.join("public/assets/#{asset_path}")
-    end
-  end
-
-  def inline_js(asset_name)
-    content_tag(:script, inline_file(asset_name), type: "text/javascript")
-  end
-
-  def inline_stylesheet(asset_name)
-    content_tag(:style, inline_file(asset_name), type: "text/css")
-  end
+  include SvgHelper
 
   def inline_svg(asset_name, classname: "svg-container")
-    content_tag(:div, inline_file(asset_name), class: classname)
+    content_tag(:div, safe_svg(inline_file(asset_name)), class: classname)
+  end
+
+  private
+
+  def inline_file(asset_name)
+    if (asset = Rails.application.assets&.find_asset(asset_name))
+      asset.source
+    else
+      asset_path = Rails.application.assets_manifest.assets[asset_name]
+      Rails.root.join("public/assets/#{asset_path}")
+    end
   end
 end
