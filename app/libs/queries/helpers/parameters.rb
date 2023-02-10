73c7adff612a8f830854e958b397bab6a743ef30
Akshay Gupta <kitallis@gmail.com>
2023-02-10 22:04:05 +0530

    refactor

--- app/libs/queries/helpers/parameters.rb
+++ app/libs/queries/helpers/parameters.rb
@@ -1,17 +1,18 @@
 class Queries::Helpers::Parameters
+  UnpermittedFilterKey = Class.new(ArgumentError)
   DEFAULT_SORT_DIRECTION = "desc"
 
   def initialize
+    @filters = {}
     @search_pattern = nil
     @limit = nil
     @offset = nil
-    @filters = {}
     @sort_column = nil
     @sort_direction = DEFAULT_SORT_DIRECTION
   end
 
-  attr_reader :limit, :offset
   attr_accessor :sort_column
+  attr_reader :limit, :offset
 
   def add_sorting(col, dir)
     @sort_column = col
@@ -28,7 +29,7 @@ class Queries::Helpers::Parameters
 
   def add_filter(col, val)
     val = [val] unless val.is_a?(Array)
-    @filters.merge!({col.to_sym => val})
+    @filters.merge!(col.to_sym => val)
   end
 
   def search_by(table_to_cols)
@@ -38,9 +39,9 @@ class Queries::Helpers::Parameters
       cols.each do |col|
         acc =
           if acc.nil?
-            col_matcher(table, col)
+            col_matcher(table, col, @search_pattern)
           else
-            acc.or(col_matcher(table, col))
+            acc.or(col_matcher(table, col, @search_pattern))
           end
       end
 
@@ -49,7 +50,7 @@ class Queries::Helpers::Parameters
   end
 
   def filter_by(allowed_cols)
-    raise ArgumentError unless @filters.keys.all? { |col| col.in? allowed_cols }
+    raise UnpermittedFilterKey unless @filters.keys.all? { |col| col.in? allowed_cols }
 
     @filters.reduce(nil) do |acc, (col, val)|
       if acc.nil?
@@ -66,7 +67,7 @@ class Queries::Helpers::Parameters
 
   private
 
-  def col_matcher(arel_table, col)
-    arel_table[col].matches("%#{@search_pattern}%")
+  def col_matcher(arel_table, col, term)
+    arel_table[col].matches("%#{term}%")
   end
 end
