9103c3847c4448c0dc13d6fec492f636fa73b6e7
Akshay Gupta <kitallis@gmail.com>
2023-02-15 21:02:12 +0530

    Fix redis uptime check

--- config/initializers/easymon.rb
+++ config/initializers/easymon.rb
@@ -7,8 +7,8 @@ Easymon::Repository.add(
 Easymon::Repository.add(
   "site-redis",
   Easymon::RedisCheck.new(
-    YAML.load_file(
-      Rails.root.join("config/redis.yml"),
+    YAML.load(
+      ERB.new(Rails.root.join("config/redis.yml").read).result,
       aliases: true
     )[Rails.env].symbolize_keys
   ),
