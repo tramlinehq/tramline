7d5bc64f5d5f5140da6bc512f49d49b0338db1e2
Akshay Gupta <kitallis@gmail.com>
2023-02-15 19:34:30 +0530

    hotfix: non-transactional migration

--- db/migrate/20230127075158_improve_bundle_identifier_index_on_app.rb
+++ db/migrate/20230127075158_improve_bundle_identifier_index_on_app.rb
@@ -2,7 +2,7 @@ class ImproveBundleIdentifierIndexOnApp < ActiveRecord::Migration[7.0]
   disable_ddl_transaction!
 
   def change
-    remove_index :apps, [:bundle_identifier, :organization_id]
+    remove_index :apps, [:bundle_identifier, :organization_id], if_exists: true
     add_index :apps, [:platform, :bundle_identifier, :organization_id], name: "index_apps_on_platform_and_bundle_id_and_org_id", unique: true, algorithm: :concurrently
   end
 end
