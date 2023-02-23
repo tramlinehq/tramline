7af3f40f6e5f739159d99cf0d3f3f44b53b46766
Akshay Gupta <kitallis@gmail.com>
2023-02-23 22:12:46 +0530

    image_tag -> inline svgs in some places

--- app/helpers/steps_helper.rb
+++ app/helpers/steps_helper.rb
@@ -1,9 +1,5 @@
 module StepsHelper
-  def show_ci_cd_provider(step)
-    step.app.ci_cd_provider.display
-  end
-
-  def show_ci_cd_channel(step)
-    step.ci_cd_channel["name"]
+  def show_ci_cd(step)
+    "#{step.app.ci_cd_provider.display} • #{step.ci_cd_channel["name"]}"
   end
 end
