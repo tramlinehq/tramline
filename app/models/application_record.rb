2ae3c1a96b96856d5f4a48290a88974fb08f9d52
Akshay Gupta <kitallis@gmail.com>
2023-02-27 13:42:31 +0530

    Localize model enums generically

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -14,4 +14,9 @@ class ApplicationRecord < ActiveRecord::Base
       create_scopes: false
     }
   end
+
+  def self.human_enum_name(enum_name, enum_value)
+    enum_i18n_key = enum_name.to_s.pluralize
+    I18n.t("activerecord.attributes.#{model_name.i18n_key}.#{enum_i18n_key}.#{enum_value}")
+  end
 end
