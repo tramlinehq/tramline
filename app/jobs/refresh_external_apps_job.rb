b1f1a737718319e810f3cf7d7aa8d2c44d050004
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-02-28 00:12:00 +0530

    Remove refresh external app periodic job in dev by deafult

--- app/jobs/refresh_external_apps_job.rb
+++ app/jobs/refresh_external_apps_job.rb
@@ -3,6 +3,7 @@ class RefreshExternalAppsJob < ApplicationJob
   queue_as :high
 
   def perform
+    return if Rails.env.development? && !ENV["REFRESH_EXTERNAL_APPS"]
     App.all.each { |app| app.refresh_external_app if app.ready? }
   end
 end
