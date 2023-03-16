69b6a39f3855eb5fc301f7a094354e1b01e61991
Akshay Gupta <kitallis@gmail.com>
2023-03-16 13:17:23 +0530

    Improve the spec

--- spec/models/accounts/user_spec.rb
+++ spec/models/accounts/user_spec.rb
@@ -16,17 +16,29 @@ describe Accounts::User do
       expect(user.organizations.size).to eq(1)
     end
 
-    it "attaches an error if the user already exists" do
-      existing_user = create(:user, :as_developer)
-      new_org = build(:organization)
-      new_user = build(:user, email: existing_user.email, organizations: [new_org])
+    context "when already exists" do
+      it "adds an error if the user already exists" do
+        existing_user = create(:user, :as_developer)
+        new_org = build(:organization)
+        new_user = build(:user, email: existing_user.email, organizations: [new_org])
 
-      user = described_class.onboard! new_user
+        user = described_class.onboard! new_user
 
-      expect(user).not_to be_persisted
-      expect(described_class.count).to eq(1)
-      expect(described_class.first.memberships.size).to eq(1)
-      expect(described_class.first.organizations.size).to eq(1)
+        expect(user).not_to be_valid
+        expect(user).not_to be_persisted
+      end
+
+      it "organizations or memberships remain unchanged" do
+        existing_user = create(:user, :as_developer)
+        new_org = build(:organization)
+        new_user = build(:user, email: existing_user.email, organizations: [new_org])
+
+        described_class.onboard! new_user
+
+        expect(described_class.first.memberships.size).to eq(1)
+        expect(described_class.first.organizations.size).to eq(1)
+        expect(described_class.count).to eq(1)
+      end
     end
   end
 end
