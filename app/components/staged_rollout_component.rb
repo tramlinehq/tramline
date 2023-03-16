6f0f418f2842ddbb020c07298d7adf29f0630550
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-03-16 20:55:17 +0530

    Add resume and pause actions to the staged rollout component

--- app/components/staged_rollout_component.rb
+++ app/components/staged_rollout_component.rb
@@ -4,8 +4,11 @@ class StagedRolloutComponent < ViewComponent::Base
   include AssetsHelper
 
   HALT_CONFIRM = "You are about to halt the rollout of this build to production, it can not be resumed.\n\nAre you sure?"
+  START_RELEASE_CONFIRM = "You are about to release this build to the first stage in production.\n\nAre you sure?"
   RELEASE_CONFIRM = "You are about to release this build to the next stage in production.\n\nAre you sure?"
   FULLY_RELEASE_CONFIRM = "You are about to release this build to all users in production.\n\nAre you sure?"
+  PAUSE_RELEASE_CONFIRM = "You are about to pause the scheduled phased release in production.\n\nAre you sure?"
+  RESUME_RELEASE_CONFIRM = "You are about to resume the scheduled phased release in production.\n\nAre you sure?"
 
   def initialize(staged_rollout)
     @staged_rollout = staged_rollout
@@ -25,53 +28,28 @@ class StagedRolloutComponent < ViewComponent::Base
     :current_stage,
     :last_rollout_percentage,
     to: :staged_rollout
-  delegate :controllable_rollout?, :rolloutable?, to: :deployment_run
+  delegate :controllable_rollout?, :rolloutable?, :automatic_rollout?, to: :deployment_run
 
-  def release_actions(form)
-    return unless controllable_rollout?
-    return if stopped? || completed?
+  def actions
+    actions = []
 
-    if failed?
-      retry_button(form)
-    elsif started?
-      increase_rollout_button(form)
-    else
-      start_rollout_button(form)
+    if controllable_rollout?
+      actions.append({form_url: increase_release_path, confirm: START_RELEASE_CONFIRM, type: :blue, name: "Start Rollout"}) if created?
+      actions.append({form_url: increase_release_path, confirm: RELEASE_CONFIRM, type: :blue, name: "Increase Rollout"}) if started?
+      actions.append({form_url: increase_release_path, confirm: RELEASE_CONFIRM, type: :blue, name: "Retry"}) if failed?
+
+      if last_rollout_percentage
+        actions.append({form_url: full_release_path, confirm: FULLY_RELEASE_CONFIRM, type: :blue, name: "Release to 100%"})
+        actions.append({form_url: halt_release_path, confirm: HALT_CONFIRM, type: :red, name: "Halt"})
+      end
     end
-  end
 
-  def halt_action(form)
-    return unless controllable_rollout?
-    return unless last_rollout_percentage
+    if automatic_rollout?
+      actions.append({form_url: pause_release_path, confirm: PAUSE_RELEASE_CONFIRM, type: :red, name: "Pause Phased Release"}) if started?
+      actions.append({form_url: resume_release_path, confirm: RESUME_RELEASE_CONFIRM, type: :red, name: "Resume Phased Release"}) if paused?
+    end
 
-    halt_rollout_button(form)
-  end
-
-  def fully_rollout_action(form)
-    return unless rolloutable?
-    return unless last_rollout_percentage
-
-    fully_rollout_button(form)
-  end
-
-  def start_rollout_button(form)
-    form.authz_submit :blue, "Start Rollout", class: "btn-xs"
-  end
-
-  def retry_button(form)
-    form.authz_submit :blue, "Retry", class: "btn-xs"
-  end
-
-  def increase_rollout_button(form)
-    form.authz_submit :blue, "Increase Rollout", class: "btn-xs"
-  end
-
-  def halt_rollout_button(form)
-    form.authz_submit :red, "Halt", class: "btn-xs"
-  end
-
-  def fully_rollout_button(form)
-    form.authz_submit :blue, "Release to 100%", class: "btn-xs"
+    actions
   end
 
   def current_stage_perc
@@ -95,25 +73,31 @@ class StagedRolloutComponent < ViewComponent::Base
   delegate :writer?, to: :helpers
   attr_reader :release, :step_run, :deployment_run, :staged_rollout
 
-  def release_form_url
-    {
-      model: [release, step_run, deployment_run, staged_rollout],
-      url: increase_deployment_run_staged_rollout_path(deployment_run)
-    }
+  def increase_release_path
+    increase_deployment_run_staged_rollout_path(deployment_run)
   end
 
-  def halt_form_url
-    {
-      model: [release, step_run, deployment_run, staged_rollout],
-      url: halt_deployment_run_staged_rollout_path(deployment_run)
-    }
+  def halt_release_path
+    halt_deployment_run_staged_rollout_path(deployment_run)
   end
 
-  def fully_release_form_url
-    {
-      model: [release, step_run, deployment_run, staged_rollout],
-      url: fully_release_deployment_run_staged_rollout_path(deployment_run)
-    }
+  def pause_release_path
+    pause_deployment_run_staged_rollout_path(deployment_run)
+  end
+
+  def resume_release_path
+    resume_deployment_run_staged_rollout_path(deployment_run)
+  end
+
+  def full_release_path
+    fully_release_deployment_run_staged_rollout_path(deployment_run)
+  end
+
+  def build_action(url, confirm, type, name)
+    {form_url: url,
+     confirm: confirm,
+     type: type,
+     name: name}
   end
 
   def badge
