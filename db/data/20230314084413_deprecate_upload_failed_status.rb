2108865d3434fee611845bc820cf059b180a1b51
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-03-16 12:21:15 +0530

    Add a class defined guard to data migration to ensure posterity

--- db/data/20230314084413_deprecate_upload_failed_status.rb
+++ db/data/20230314084413_deprecate_upload_failed_status.rb
@@ -1,5 +1,7 @@
 class DeprecateUploadFailedStatus < ActiveRecord::Migration[7.0]
   def up
+    return unless defined?(DeploymentRun)
+
     DeploymentRun.upload_failed.each do |run|
       run.update(failure_reason: :unknown_failure, status: :failed)
     end
