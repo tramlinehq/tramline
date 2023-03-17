cae91f76b2dd53942d041d9666742116f203839a
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-03-17 10:46:57 +0530

    Increase update external release job retries to go upto 10 days

--- app/jobs/deployments/app_store_connect/update_external_release_job.rb
+++ app/jobs/deployments/app_store_connect/update_external_release_job.rb
@@ -4,7 +4,7 @@ class Deployments::AppStoreConnect::UpdateExternalReleaseJob
   extend Backoffable
 
   queue_as :high
-  sidekiq_options retry: 14
+  sidekiq_options retry: 75
 
   sidekiq_retry_in do |count, ex|
     if ex.is_a?(Deployments::AppStoreConnect::Release::ExternalReleaseNotInTerminalState)
