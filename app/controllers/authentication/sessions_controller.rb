3a931eec992565a2d0d63bd51893bd3d86e0f3fd
Akshay Gupta <kitallis@gmail.com>
2023-03-21 11:30:51 +0530

    Add explicit destroy hook in sessions controller

--- app/controllers/authentication/sessions_controller.rb
+++ app/controllers/authentication/sessions_controller.rb
@@ -1,6 +1,10 @@
 class Authentication::SessionsController < Devise::SessionsController
   after_action :prepare_intercom_shutdown, only: [:destroy]
 
+  def destroy
+    super
+  end
+
   protected
 
   def prepare_intercom_shutdown
