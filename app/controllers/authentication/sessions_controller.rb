3cc3420f1bc7ebadbf4a99caa96331ffcc560e38
Akshay Gupta <kitallis@gmail.com>
2023-03-21 11:56:36 +0530

    Shutdown intercom

--- app/controllers/authentication/sessions_controller.rb
+++ app/controllers/authentication/sessions_controller.rb
@@ -1,5 +1,10 @@
 class Authentication::SessionsController < Devise::SessionsController
   after_action :prepare_intercom_shutdown, only: [:destroy]
+  after_action :intercom_shutdown, only: [:new]
+
+  def new
+    super
+  end
 
   def destroy
     super
@@ -9,5 +14,9 @@ class Authentication::SessionsController < Devise::SessionsController
 
   def prepare_intercom_shutdown
     IntercomRails::ShutdownHelper.prepare_intercom_shutdown(session)
+  end
+
+  def intercom_shutdown
+    IntercomRails::ShutdownHelper.intercom_shutdown(session, cookies, request.domain)
   end
 end
