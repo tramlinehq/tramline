9d6e2635e47d855e657aa5b7f0c7d6ca09c8f06a
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-03-31 11:28:06 +0530

    Change env variables for Bitrise to be same as other CI integrations

--- app/libs/installations/bitrise/api.rb
+++ app/libs/installations/bitrise/api.rb
@@ -55,8 +55,8 @@ module Installations
             commit_hash: commit_hash,
             workflow_id: workflow_id,
             environments: [
-              {mapped_to: "BUILD_VERSION", value: inputs[:build_version]},
-              {mapped_to: "BUILD_NUMBER", value: inputs[:version_code]}
+              {mapped_to: "versionName", value: inputs[:build_version]},
+              {mapped_to: "versionCode", value: inputs[:version_code]}
             ]
           },
 
