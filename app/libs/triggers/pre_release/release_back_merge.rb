7e9cdf32eb359874e7fad4e1147f19b4082ca0bf
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-05-17 20:47:27 +0530

    Fix a missed rename

--- app/libs/triggers/pre_release/release_back_merge.rb
+++ app/libs/triggers/pre_release/release_back_merge.rb
@@ -2,7 +2,7 @@ class Triggers::PreRelease
   class ReleaseBackMerge
     def self.call(release, release_branch)
       # ReleaseBackMerge behaves the same as AlmostTrunk while making a new release
-      Triggers::Release::AlmostTrunk.call(release, release_branch)
+      Triggers::PreRelease::AlmostTrunk.call(release, release_branch)
     end
   end
 end
