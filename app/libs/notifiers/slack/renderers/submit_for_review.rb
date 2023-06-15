3d896f1ab027e8295ce41b2e1f665a688e73c7a6
Akshay Gupta <kitallis@gmail.com>
2023-06-15 21:41:59 +0530

    Fix a couple of notification helper text

--- app/libs/notifiers/slack/renderers/submit_for_review.rb
+++ app/libs/notifiers/slack/renderers/submit_for_review.rb
@@ -2,36 +2,36 @@ module Notifiers
   module Slack
     class Renderers::SubmitForReview < Renderers::Base
       TEMPLATE_FILE = "submit_for_review.json.erb".freeze
-    end
 
-    def initialize(**params)
-      super(**params)
-      @publishing_text = publishing_text
-      @company_text = company_text
-      @store_text = store_text
-    end
-
-    def publishing_text
-      if @is_play_store_production
-        "managed publishing"
-      elsif @is_app_store_production
-        "manual publishing"
+      def initialize(**params)
+        super(**params)
+        @publishing_text = publishing_text
+        @company_text = company_text
+        @store_text = store_text
       end
-    end
 
-    def company_text
-      if @is_play_store_production
-        "Google"
-      elsif @is_app_store_production
-        "Apple"
+      def publishing_text
+        if @is_play_store_production
+          "managed publishing"
+        elsif @is_app_store_production
+          "manual publishing"
+        end
       end
-    end
 
-    def store_text
-      if @is_play_store_production
-        "Play Store"
-      elsif @is_app_store_production
-        "App Store"
+      def company_text
+        if @is_play_store_production
+          "Google"
+        elsif @is_app_store_production
+          "Apple"
+        end
+      end
+
+      def store_text
+        if @is_play_store_production
+          "Play Store"
+        elsif @is_app_store_production
+          "App Store"
+        end
       end
     end
   end
