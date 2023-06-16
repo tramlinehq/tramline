4e626f333d4eaab3139dde1832da18cef2761e9c
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-06-16 23:00:07 +0530

    remove commit listener spec

--- spec/factories/commit_listeners.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :commit_listener, class: "CommitListener" do
-    association :train
-    branch_name { "feat/new_story" }
-  end
-end
--- spec/models/commit_listener_spec.rb
+++
@@ -1,7 +0,0 @@
-require "rails_helper"
-
-describe CommitListener do
-  it "has valid factory" do
-    expect(create(:commit_listener)).to be_valid
-  end
-end
