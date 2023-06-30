e9d351d8bb8822f70cbee4621d08b7eac3573a90
Akshay Gupta <kitallis@gmail.com>
2023-06-30 20:06:39 +0530

    Display store logos above the store status sections

--- app/helpers/apps_helper.rb
+++ app/helpers/apps_helper.rb
@@ -3,6 +3,11 @@ module AppsHelper
     is_completed ? "bg-green-400" : "bg-blue-200"
   end
 
+  def store_logo(app)
+    return "integrations/logo_app_store.png" if app.ios?
+    "integrations/logo_google_play_store.png" if app.android?
+  end
+
   MOVEMENT_STATUS_COLORS = {
     in_progress: "bg-amber-500",
     done: "bg-indigo-500",
