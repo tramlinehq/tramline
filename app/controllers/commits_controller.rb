144ff8a9a2fd3acd178ca9d69c96634a309f39ce
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-06-30 10:10:59 +0530

    Add a check for locked release before applying commit

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -4,10 +4,15 @@ class CommitsController < SignedInApplicationController
 
   def apply
     commit = Commit.find(params[:id])
-    commit.trigger_step_runs_for(release_platform_run)
 
-    redirect_to live_release_app_train_releases_path(release.app, release.train),
-      notice: "Steps have been triggered for the commit."
+    if release_platform_run.on_track?
+      commit.trigger_step_runs_for(release_platform_run)
+      redirect_to live_release_app_train_releases_path(release.app, release.train),
+        notice: "Steps have been triggered for the commit."
+    else
+      redirect_to live_release_app_train_releases_path(release.app, release.train),
+        error: "Cannot apply a commit to a locked release."
+    end
   end
 
   private
