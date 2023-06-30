5ad84f65d359797151c883b620ed6a5a1eb83e76
Akshay Gupta <kitallis@gmail.com>
2023-06-30 20:21:25 +0530

    Dynamically show step platform subtitle

--- app/helpers/steps_helper.rb
+++ app/helpers/steps_helper.rb
@@ -2,4 +2,8 @@ module StepsHelper
   def show_ci_cd(step)
     "#{step.app.ci_cd_provider.display} • #{step.ci_cd_channel["name"]}"
   end
+
+  def platform_subtitle(app, step)
+    "For the #{step.release_platform.display_attr(:platform)} platform" if app.cross_platform?
+  end
 end
