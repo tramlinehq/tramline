8ce82428edb2ac83d87d810fbdef174f72a54d02
Akshay Gupta <kitallis@gmail.com>
2023-06-30 19:44:54 +0530

    Subtitles in page components

--- app/components/page_component.rb
+++ app/components/page_component.rb
@@ -1,10 +1,12 @@
 class PageComponent < ViewComponent::Base
   renders_many :actions
+  include AssetsHelper
 
-  def initialize(title:, breadcrumb: nil)
+  def initialize(title:, breadcrumb: nil, subtitle: nil)
     @breadcrumb = breadcrumb
     @page_title = title
+    @subtitle = subtitle
   end
 
-  attr_reader :breadcrumb, :page_title
+  attr_reader :breadcrumb, :page_title, :subtitle
 end
