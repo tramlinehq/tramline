19ed84e561a217c0a16c7dec0fa6a92d6f8577e4
Akshay Gupta <kitallis@gmail.com>
2023-06-30 12:31:39 +0530

    Fix migration typo

--- db/migrate/20230628115739_add_platform_specific_integration_config.rb
+++ db/migrate/20230628115739_add_platform_specific_integration_config.rb
@@ -1,9 +1,9 @@
 class AddPlatformSpecificIntegrationConfig < ActiveRecord::Migration[7.0]
   def change
     safety_assured do
-      change_table :deployments, bulk: true do |t|
-        add_column :app_configs, :firebase_ios_config, :jsonb, null: true
-        add_column :app_configs, :firebase_android_config, :jsonb, null: true
+      change_table :app_configs, bulk: true do |t|
+        t.column :firebase_ios_config, :jsonb, null: true
+        t.column :firebase_android_config, :jsonb, null: true
       end
     end
   end
