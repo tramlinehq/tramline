912e8e6a4cd9bf455817ffc7f773d13d808a91fb
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-07-04 18:12:05 +0530

    minor

--- app/models/train.rb
+++ app/models/train.rb
@@ -148,6 +148,10 @@ class Train < ApplicationRecord
     BRANCHING_STRATEGIES[branching_strategy.to_sym]
   end
 
+  def build_channel_integrations
+    integrations.build_channel
+  end
+
   def bump_release!(has_major_bump = false)
     bump_term = has_major_bump ? :major : :minor
 
@@ -201,10 +205,6 @@ class Train < ApplicationRecord
         train_current_version: version_current
       }
     )
-  end
-
-  def build_channel_integrations
-    integrations.build_channel
   end
 
   private
