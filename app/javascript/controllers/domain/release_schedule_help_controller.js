bf3113c1a3522b27feee193a0e88e86686ba0853
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-08-07 20:41:43 +0530

    UI validations on release schdeule

--- app/javascript/controllers/domain/release_schedule_help_controller.js
+++ app/javascript/controllers/domain/release_schedule_help_controller.js
@@ -1,21 +1,35 @@
 import {Controller} from "@hotwired/stimulus";
 
-const NEXT_RELEASE = "Your next release after the initial kickoff will be on: "
+const NEXT_RELEASE = "Your next release after the initial kickoff will be on – "
 
 export default class extends Controller {
-  static targets = ["kickoffDate", "nextDateNumber", "nextDateUnit", "output"];
+  static targets = ["kickoffDate", "nextDateNumber", "nextDateUnit", "output", "errOutput"];
 
   initialize() {
     this.change();
   }
 
   change() {
+    this.__resetContents()
+
     if (this.__isEmptyInput()) {
       return;
     }
+
     const nextDateNumber = parseInt(this.nextDateNumberTarget.value);
-    const nextDateUnit = this.nextDateUnitTarget.value;
     const kickoffDate = new Date(this.kickoffDateTarget.value);
+
+    if (this.__invalidRepeatDuration(nextDateNumber)) {
+      this.errOutputTarget.textContent = "Invalid repeat duration, please choose a duration >= 1 day & <= 365 days"
+      return
+    }
+
+    if (this.__invalidKickoffDate(kickoffDate)) {
+      this.errOutputTarget.textContent = "The initial scheduled kickoff for the train should be in the future"
+      return
+    }
+
+    const nextDateUnit = this.nextDateUnitTarget.value;
     const nextDate = new Date(kickoffDate);
 
     switch (nextDateUnit) {
@@ -43,6 +57,11 @@ export default class extends Controller {
     this.nextDateNumberTarget.value = null
   }
 
+  __resetContents() {
+    this.outputTarget.textContent = ""
+    this.errOutputTarget.textContent = ""
+  }
+
   __isEmptyInput() {
     return this.kickoffDateTarget.value === "" ||
       this.nextDateUnitTarget.value === "" || this.nextDateNumberTarget.value === ""
@@ -50,5 +69,17 @@ export default class extends Controller {
 
   __isValidDate(d) {
     return d instanceof Date && !isNaN(d);
+  }
+
+  __invalidKickoffDate(d) {
+    return d < new Date()
+  }
+
+  __invalidRepeatDuration(n) {
+    if (typeof n !== 'number') {
+      return true
+    }
+
+    return n < 1 || n > 365
   }
 }
