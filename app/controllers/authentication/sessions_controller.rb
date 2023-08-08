3c22f4b0134c8be914c8a1f0466ec78539b9812d
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-08-08 21:25:39 +0530

    Redirect user to original page after sign in (#503)

--- app/controllers/authentication/sessions_controller.rb
+++ app/controllers/authentication/sessions_controller.rb
@@ -10,6 +10,10 @@ class Authentication::SessionsController < Devise::SessionsController
     super
   end
 
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || root_path
+  end
+
   protected
 
   def prepare_intercom_shutdown
