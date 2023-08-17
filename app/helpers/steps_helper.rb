849c83835f73e7bc03e69b38663f9f07269cfaba
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-08-17 18:27:21 +0530

    Show auto deploy off on step viz

--- app/helpers/steps_helper.rb
+++ app/helpers/steps_helper.rb
@@ -6,4 +6,12 @@ module StepsHelper
   def platform_subtitle(app, step)
     "For the #{step.release_platform.display_attr(:platform)} platform" if app.cross_platform?
   end
+
+  def auto_deploy_status_badge(step)
+    step.auto_deploy? ? "" : "Manual Distribution"
+  end
+
+  def auto_deploy_status(step)
+    step.auto_deploy? ? "ON" : "OFF"
+  end
 end
