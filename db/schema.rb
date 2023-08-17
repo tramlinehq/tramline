9105a664d8244a1ececf981a9d2a555fbb48fe13
Akshay Gupta <kitallis@gmail.com>
2023-08-17 20:12:06 +0530

    Add external stripe billing management

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_10_055850) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_17_135023) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "pgcrypto"
@@ -271,6 +271,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_10_055850) do
     t.string "created_by", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "subscribed", default: false
     t.index ["slug"], name: "index_organizations_on_slug", unique: true
     t.index ["status"], name: "index_organizations_on_status"
   end
