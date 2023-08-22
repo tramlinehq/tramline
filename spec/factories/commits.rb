f64c5ce17f9fb7a3188fc3e66e21f4f629744a62
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-08-22 21:34:35 +0530

    Build queue specs

--- spec/factories/commits.rb
+++ spec/factories/commits.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :commit do
     commit_hash { SecureRandom.uuid.split("-").join }
     release { association :release }
+    build_queue { association :build_queue }
     message { "feat: introduce commit listener" }
     timestamp { "2022-06-21 20:20:21" }
     author_name { "Jon Doe" }
