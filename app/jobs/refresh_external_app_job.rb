97eecc2c0920793846dba7207595fea1a1af01ee
Akshay Gupta <kitallis@gmail.com>
2023-08-29 19:24:39 +0530

    Skip the deadset for RefreshExternalAppJob failures

--- app/jobs/refresh_external_app_job.rb
+++ app/jobs/refresh_external_app_job.rb
@@ -1,6 +1,7 @@
 class RefreshExternalAppJob < ApplicationJob
   include Loggable
   queue_as :high
+  sidekiq_options retry: 0, dead: false # skip DLQ
 
   def perform(app_id)
     app = App.find(app_id)
