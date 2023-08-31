838e8ae027ff7ef02a445dfacc0a2892adc227f7
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-08-31 19:51:02 +0530

    Limit build notes size for testflight (#528)

--- app/models/release_metadata.rb
+++ app/models/release_metadata.rb
@@ -16,10 +16,11 @@ class ReleaseMetadata < ApplicationRecord
 
   belongs_to :release, inverse_of: :release_metadata
 
+  NOTES_MAX_LENGTH = 4000
   DEFAULT_LOCALE = "en-US"
   DEFAULT_RELEASE_NOTES = "The latest version contains bug fixes and performance improvements."
   PLAINTEXT_REGEX = /\A[!@#$%^&*()_+\-=\[\]{};':"\\|,.\/?\s\p{Alnum}\p{P}\p{Zs}\p{Emoji_Presentation}]+\z/
 
-  validates :release_notes, format: {with: PLAINTEXT_REGEX, message: :no_special_characters, multiline: true}, length: {maximum: 4000}
+  validates :release_notes, format: {with: PLAINTEXT_REGEX, message: :no_special_characters, multiline: true}, length: {maximum: NOTES_MAX_LENGTH}
   validates :promo_text, format: {with: PLAINTEXT_REGEX, message: :no_special_characters, allow_blank: true, multiline: true}, length: {maximum: 170}
 end
