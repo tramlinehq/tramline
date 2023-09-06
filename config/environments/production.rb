298d795e2de147a6983c7448742afa79beed4ee3
Akshay Gupta <kitallis@gmail.com>
2023-09-06 19:28:02 +0530

    Avoid HOST header manipulation attacks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,10 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  # Avoid cache poisoning attack by users setting X-Forwarded-Host
+  config.hosts << ENV["HOST_NAME"]
+  config.hosts << ".#{ENV["HOST_NAME"]}"
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
