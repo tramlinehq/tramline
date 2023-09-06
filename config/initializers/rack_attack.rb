bfa3431846dd9360ca2e634f4e585890803e0f82
Akshay Gupta <kitallis@gmail.com>
2023-09-06 19:04:43 +0530

    Rate-limit forgot your passsword requests

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,11 @@
+# config/initializers/rack_attack.rb
+
+Rack::Attack.throttled_responder = lambda do |_request|
+  [429, {}, ["Too many requests. Please wait and try again later.\n"]]
+end
+
+Rack::Attack.throttle("forgot_password_requests_by_email", limit: 3, period: 1.hour) do |req|
+  if req.path == "/users/password" && req.post? && req.params["user"].present?
+    req.params["user"]["email"].to_s.downcase # Use the email address as a discriminator
+  end
+end
