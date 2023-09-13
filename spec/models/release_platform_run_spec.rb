4fd6fa36ab4673b0c18ee86b04be6c67a7635c35
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-09-13 22:03:18 +0530

    specs

--- spec/models/release_platform_run_spec.rb
+++ spec/models/release_platform_run_spec.rb
@@ -411,7 +411,7 @@ describe ReleasePlatformRun do
   describe "#on_finish!" do
     it "schedules a platform-specific tag job if cross-platform app" do
       app = create(:app, :cross_platform)
-      train = create(:train, app:)
+      train = create(:train, app:, tag_platform_releases: true)
       release = create(:release, train:)
       release_platform = create(:release_platform, train:)
       release_platform_run = create(:release_platform_run, :on_track, release:, release_platform:)
@@ -420,6 +420,19 @@ describe ReleasePlatformRun do
       release_platform_run.finish!
 
       expect(ReleasePlatformRuns::CreateTagJob).to have_received(:perform_later).with(release_platform_run.id).once
+    end
+
+    it "does not schedule a platform-specific tag job if cross-platform app tagging all store releases" do
+      app = create(:app, :cross_platform)
+      train = create(:train, app:, tag_platform_releases: true, tag_all_store_releases: true)
+      release = create(:release, train:)
+      release_platform = create(:release_platform, train:)
+      release_platform_run = create(:release_platform_run, :on_track, release:, release_platform:)
+      allow(ReleasePlatformRuns::CreateTagJob).to receive(:perform_later)
+
+      release_platform_run.finish!
+
+      expect(ReleasePlatformRuns::CreateTagJob).not_to have_received(:perform_later).with(release_platform_run.id)
     end
 
     it "does not schedule a platform-specific tag job for single-platform apps" do
