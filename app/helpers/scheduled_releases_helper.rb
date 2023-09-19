346caf040aa092823741274efc44d95f7d1ca7da
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-09-19 16:40:07 +0530

    Show scheduled releases history on train page

---
+++ app/helpers/scheduled_releases_helper.rb
@@ -0,0 +1,19 @@
+module ScheduledReleasesHelper
+  def scheduled_release_badge(scheduled_release)
+    if scheduled_release.is_success?
+      status_badge("success", :success)
+    elsif !scheduled_release.pending?
+      status_badge("skipped", :neutral)
+    else
+      status_badge("scheduled", :ongoing)
+    end
+  end
+
+  def scheduled_release_text(scheduled_release)
+    if scheduled_release.pending?
+      "Scheduled for #{time_format(scheduled_release.scheduled_at)}"
+    else
+      "Ran on #{time_format(scheduled_release.scheduled_at)}"
+    end
+  end
+end
