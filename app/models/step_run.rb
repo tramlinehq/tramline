8fbffc641b01afb77dad9f4e91be9a8e583ad4b1
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-09-26 16:49:46 +0530

    Use release changelog for build notes only if present

--- app/models/step_run.rb
+++ app/models/step_run.rb
@@ -295,8 +295,8 @@ class StepRun < ApplicationRecord
   end
 
   def relevant_changes
-    if release.first_commit == commit
-      return release.release_changelog&.commit_messages
+    if release.first_commit == commit && release.release_changelog.present?
+      return release.release_changelog.commit_messages
     end
     release_platform_run.commit_messages_before(self)
   end
