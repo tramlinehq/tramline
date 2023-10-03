de4ab47a6a96ba28f3c8cb39b2be33e55264ac6f
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-10-03 19:53:02 +0530

    Add icons for all notifications

--- app/components/notification_settings_component.rb
+++ app/components/notification_settings_component.rb
@@ -9,21 +9,21 @@ class NotificationSettingsComponent < ViewComponent::Base
   InvalidNotificationSettings = Class.new(StandardError)
 
   NOTIFICATIONS = {
-    release_scheduled: {number: 1, description: "Your scheduled release will run in a few hours."},
-    release_started: {number: 2, description: "A new release was started for the release train."},
-    step_started: {number: 3, description: "A step was started for the release train."},
-    step_failed: {number: 4, description: "A step failed to run fully for the release train."},
-    backmerge_failed: {number: 5, description: "Tramline failed to create a backmerge PR for the commit in the release."},
-    submit_for_review: {number: 6, description: "A build was submitted for review to store for the release."},
-    review_approved: {number: 7, description: "A production build review was approved by the store."},
-    staged_rollout_updated: {number: 8, description: "The staged rollout was increased for the production build in the store."},
-    staged_rollout_paused: {number: 9, description: "The staged rollout was paused for the production build in the store."},
-    staged_rollout_resumed: {number: 10, description: "The staged rollout was resumed for the production build in the store."},
-    staged_rollout_halted: {number: 11, description: "The staged rollout was halted for the production build in the store."},
-    staged_rollout_fully_released: {number: 12, description: "The staged rollout was fully released to 100% for the production build in the store."},
-    deployment_finished: {number: 13, description: "The distribution was successful to a channel."},
-    release_ended: {number: 14, description: "The release finished successfully."},
-    release_stopped: {number: 15, description: "The release was stopped before it finished."}
+    release_scheduled: {number: 1, icon: "clock.svg", description: "Your scheduled release will run in a few hours."},
+    release_started: {number: 2, icon: "bolt.svg", description: "A new release was started for the release train."},
+    step_started: {number: 3, icon: "paper_airplane.svg", description: "A step was started for the release train."},
+    step_failed: {number: 4, icon: "failure.svg", description: "A step failed to run fully for the release train."},
+    backmerge_failed: {number: 5, icon: "band_aid.svg", description: "Tramline failed to create a backmerge PR for the commit in the release."},
+    submit_for_review: {number: 6, icon: "clipboard_copy.svg", description: "A build was submitted for review to store for the release."},
+    review_approved: {number: 7, icon: "clipboard_check.svg", description: "A production build review was approved by the store."},
+    staged_rollout_updated: {number: 8, icon: "chart.svg", description: "The staged rollout was increased for the production build in the store."},
+    staged_rollout_paused: {number: 9, icon: "pause.svg", description: "The staged rollout was paused for the production build in the store."},
+    staged_rollout_resumed: {number: 10, icon: "play.svg", description: "The staged rollout was resumed for the production build in the store."},
+    staged_rollout_halted: {number: 11, icon: "halt.svg", description: "The staged rollout was halted for the production build in the store."},
+    staged_rollout_fully_released: {number: 12, icon: "fast_forward.svg", description: "The staged rollout was fully released to 100% for the production build in the store."},
+    deployment_finished: {number: 13, icon: "truck_delivery.svg", description: "The distribution was successful to a channel."},
+    release_ended: {number: 14, icon: "sparkles.svg", description: "The release finished successfully."},
+    release_stopped: {number: 15, icon: "close_icon.svg", description: "The release was stopped before it finished."}
   }.with_indifferent_access
 
   unless Set.new(NOTIFICATIONS.keys).eql?(Set.new(NotificationSetting.kinds.keys))
@@ -45,5 +45,9 @@ class NotificationSettingsComponent < ViewComponent::Base
 
   def description_for(setting)
     NOTIFICATIONS[setting.kind][:description]
+  end
+
+  def icon_for(setting)
+    NOTIFICATIONS[setting.kind][:icon] || "aerial_lift.svg"
   end
 end
