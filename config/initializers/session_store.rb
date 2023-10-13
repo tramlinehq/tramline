592fa2244b88137d5e3120254cfb439d0c3e0c0e
Akshay Gupta <kitallis@gmail.com>
2023-10-13 14:51:30 +0530

    Explicit session redis url

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 if ENV["RAILS_PIPELINE_ENV"].eql?("staging")
   Rails.application.config.session_store :redis_store,
-    servers: [ENV["DEFAULT_REDIS_URL"]],
+    servers: [ENV["SESSION_REDIS_URL"]],
     expire_after: (ENV["SESSION_TIMEOUT_IN_MINUTES"]&.to_i&.minutes || 5.days),
     key: "_tramline_site_session",
     threadsafe: true,
