a8fbe45a748980b3b5751c9acfab552d340bd4fc
Akshay Gupta <kitallis@gmail.com>
2023-11-29 12:45:56 +0530

    button components with blocks

--- app/components/v2/button_component.rb
+++ app/components/v2/button_component.rb
@@ -5,22 +5,22 @@ class V2::ButtonComponent < V2::BaseComponent
   BASE_OPTS = "btn group"
   BUTTON_OPTIONS = {
     default: {
-      class: "#{BASE_OPTS} text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      class: "#{BASE_OPTS} text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
     },
     dark: {
-      class: "#{BASE_OPTS} text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+      class: "#{BASE_OPTS} text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
     },
     light: {
       class: "#{BASE_OPTS} text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
     },
     primary: {
-      class: "#{BASE_OPTS} text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+      class: "#{BASE_OPTS} text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
     },
     danger: {
-      class: "#{BASE_OPTS} text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+      class: "#{BASE_OPTS} text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
     },
     neutral: {
-      class: "#{BASE_OPTS} text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm text-center me-2 mb-2 dark:focus:ring-yellow-900"
+      class: "#{BASE_OPTS} text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm text-center dark:focus:ring-yellow-900"
     },
     disabled:
       {
@@ -38,7 +38,7 @@ class V2::ButtonComponent < V2::BaseComponent
     xl: "px-6 py-3.5 text-base"
   }
 
-  def initialize(label: "", scheme: :default, type: :button, visual_icon: nil, size: :base, options: nil, html_options: nil)
+  def initialize(label: nil, scheme: :default, type: :button, visual_icon: nil, size: :base, options: nil, html_options: nil)
     raise ArgumentError, "Invalid scheme" unless SCHEMES.include?(scheme)
     raise ArgumentError, "Invalid button type" unless TYPES.include?(type)
     raise ArgumentError, "Invalid size" unless SIZES.keys.include?(size)
@@ -53,32 +53,37 @@ class V2::ButtonComponent < V2::BaseComponent
   end
 
   def before_render
-    @options, @html_options = apply_button_styles(@options, @html_options)
+    @html_options = apply_html_options(@html_options)
   end
 
   def link_to_component
+    classname = ""
+    classname = "ml-2" unless icon_only?
+
     link_to(@options, @html_options) do
       concat icon
 
       if content
-        concat content
+        concat content_tag(:span, content, class: classname)
       else
-        if @label && !icon_only?
-          concat content_tag(:span, @label, class: "ml-2")
+        if @label
+          concat content_tag(:span, @label, class: classname)
         end
       end
     end
   end
 
   def button_to_component
+    classname = ""
+    classname = "ml-2" unless icon_only?
+
     button_to(@options, @html_options) do
       concat icon
-
       if content
-        concat content
+        concat content_tag(:span, content, class: classname)
       else
-        if @label && !icon_only?
-          concat content_tag(:span, @label, class: "ml-2")
+        if @label
+          concat content_tag(:span, @label, class: classname)
         end
       end
     end
@@ -91,25 +96,14 @@ class V2::ButtonComponent < V2::BaseComponent
       content_tag(:span, "Processing...", class: "hidden group-disabled:inline group-disabled:opacity-60")
   end
 
-  def apply_button_options(options, new_options)
+  def apply_html_options(options)
+    new_options = BUTTON_OPTIONS[scheme]
     options ||= {}
     options[:class] ||= "".dup
     options[:class] << " #{new_options[:class]}"
     options[:class] << " #{SIZES[@size]}"
     options[:class].squish
     options.merge(new_options.except(:class))
-  end
-
-  def apply_button_styles(options, html_options)
-    new_opts = BUTTON_OPTIONS[scheme]
-
-    if content
-      options = apply_button_options(options, new_opts)
-    else
-      html_options = apply_button_options(html_options, new_opts)
-    end
-
-    [options, html_options]
   end
 
   def link?
@@ -130,10 +124,6 @@ class V2::ButtonComponent < V2::BaseComponent
   end
 
   def icon_only?
-    !content && @visual_icon
-  end
-
-  def icon_only_restructure
-    "" if icon_only?
+    !@label && !content && @visual_icon
   end
 end
