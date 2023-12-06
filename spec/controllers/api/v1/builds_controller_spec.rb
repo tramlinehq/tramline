314f9f1168e34874ebec7f2223ffc27113f7d69e
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-12-06 17:16:58 +0530

    controller failure specs

--- spec/controllers/api/v1/builds_controller_spec.rb
+++ spec/controllers/api/v1/builds_controller_spec.rb
@@ -43,37 +43,62 @@ describe Api::V1::BuildsController do
     ]
   }
 
-  before do
-    request.headers["HTTP_X_TRAMLINE_ACCOUNT_ID"] = organization.id
-    request.headers["Authorization"] = "Bearer #{organization.api_key}"
+  context "when unauthorized" do
+    it "return unauthorized" do
+      patch :external_metadata,
+        format: :json,
+        params: {app_id: app.slug, version_name: step_run.build_version, version_code: step_run.build_number, external_metadata: metadata_params}
+      expect(response).to have_http_status(:unauthorized)
+    end
   end
 
-  it "creates the external build metadata for the step run" do
-    patch :external_metadata,
-      format: :json,
-      params: {app_id: app.slug, version_name: step_run.build_version, version_code: step_run.build_number, external_metadata: metadata_params}
-    expect(response).to have_http_status(:success)
-    expect(response.parsed_body["external_build_metadata"]["metadata"].values).to match_array(metadata_params.map(&:with_indifferent_access))
+  context "when invalid build" do
+    before do
+      request.headers["HTTP_X_TRAMLINE_ACCOUNT_ID"] = organization.id
+      request.headers["Authorization"] = "Bearer #{organization.api_key}"
+    end
+
+    it "return not found" do
+      patch :external_metadata,
+        format: :json,
+        params: {app_id: app.slug, version_name: "invalid", version_code: step_run.build_number, external_metadata: metadata_params}
+      expect(response).to have_http_status(:not_found)
+    end
   end
 
-  it "updates the external build metadata for the step run" do
-    single_metadata = [
-      {
-        identifier: "app_launch_time",
-        name: "App Launch Time",
-        description: "This is the time in seconds for the app to start",
-        value: "1.5",
-        type: "number",
-        unit: "seconds"
-      }
-    ]
-    old_metadata = create(:external_build_metadata, step_run:)
-    patch :external_metadata,
-      format: :json,
-      params: {app_id: app.slug, version_name: step_run.build_version, version_code: step_run.build_number, external_metadata: single_metadata}
-    expect(response).to have_http_status(:success)
-    new_metadata = response.parsed_body["external_build_metadata"]
-    expect(new_metadata.dig("metadata", "app_launch_time")).to eq(single_metadata.find { |m| m[:identifier] == "app_launch_time" }.with_indifferent_access)
-    expect(new_metadata.dig("metadata", "unit_test_coverage")).to eq(old_metadata.metadata.fetch("unit_test_coverage"))
+  context "when authorized" do
+    before do
+      request.headers["HTTP_X_TRAMLINE_ACCOUNT_ID"] = organization.id
+      request.headers["Authorization"] = "Bearer #{organization.api_key}"
+    end
+
+    it "creates the external build metadata for the step run" do
+      patch :external_metadata,
+        format: :json,
+        params: {app_id: app.slug, version_name: step_run.build_version, version_code: step_run.build_number, external_metadata: metadata_params}
+      expect(response).to have_http_status(:success)
+      expect(response.parsed_body["external_build_metadata"]["metadata"].values).to match_array(metadata_params.map(&:with_indifferent_access))
+    end
+
+    it "updates the external build metadata for the step run" do
+      single_metadata = [
+        {
+          identifier: "app_launch_time",
+          name: "App Launch Time",
+          description: "This is the time in seconds for the app to start",
+          value: "1.5",
+          type: "number",
+          unit: "seconds"
+        }
+      ]
+      old_metadata = create(:external_build_metadata, step_run:)
+      patch :external_metadata,
+        format: :json,
+        params: {app_id: app.slug, version_name: step_run.build_version, version_code: step_run.build_number, external_metadata: single_metadata}
+      expect(response).to have_http_status(:success)
+      new_metadata = response.parsed_body["external_build_metadata"]
+      expect(new_metadata.dig("metadata", "app_launch_time")).to eq(single_metadata.find { |m| m[:identifier] == "app_launch_time" }.with_indifferent_access)
+      expect(new_metadata.dig("metadata", "unit_test_coverage")).to eq(old_metadata.metadata.fetch("unit_test_coverage"))
+    end
   end
 end
