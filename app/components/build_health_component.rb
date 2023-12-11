452d3adeb10d9e3d8f66741626494fd87ce450a0
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-12-11 11:17:01 +0530

    query and chart app size and external build metadata

--- app/components/build_health_component.rb
+++ app/components/build_health_component.rb
@@ -11,32 +11,47 @@ class BuildHealthComponent < ViewComponent::Base
     release_platform_run.last_run_for(step)
   end
 
+  def step_runs
+    @step_runs ||= release_platform_run.step_runs_for(step)
+  end
+
+  def chartable_metadata
+    ["app_size"].concat release_platform_run.external_builds.last.metadata.filter { |_k, v| v["type"] == "number" }.keys
+  end
+
+  def app_size_data
+    {"app_size" => {
+      identifier: "app_size",
+      name: "App Size",
+      description: "",
+      type: "number",
+      unit: "MB",
+      data: step_runs.map { |srun| [srun.build_number, {value: srun.build_size}] }.to_h
+    }}
+  end
+
   def health_data
-    {
-      app_launch_time: {
-        identifier: "app_launch_time",
-        name: "App Launch Time",
-        description: "This is the time in seconds for the app to start",
-        type: "number",
-        unit: "seconds",
-        data: { "123": { value: 1.5 }, "124": { value: 1.0 }, "125": { value: 1.2 }, "126": { value: 1.1 } } },
-      unit_test_coverage: {
-        identifier: "unit_test_coverage",
-        name: "Unit Test Coverage",
-        description: "This is the time in seconds for the app to start",
-        type: "number",
-        unit: "seconds",
-        data: { "123": { value: 50 }, "124": { value: 55 }, "125": { value: 50 }, "126": { value: 45 } }
-      },
-      mint_coverage: {
-        identifier: "mint_coverage",
-        name: "Mint Coverage",
-        description: "This is the time in seconds for the app to start",
-        type: "number",
-        unit: "seconds",
-        data: { "123": { value: 90 }, "124": { value: 85 }, "125": { value: 80 }, "126": { value: 85 } }
-      }
-    }
+    return unless step_runs.size > 1
+
+    step_runs.each_with_object(app_size_data) do |step_run, acc|
+      metadata = step_run.external_build&.metadata
+      next unless metadata
+
+      metadata.each do |identifier, data|
+        next unless data["type"] == "number"
+
+        acc[identifier] ||= {
+          identifier: data["identifier"],
+          name: data["name"],
+          description: data["description"],
+          type: data["type"],
+          unit: data["unit"],
+          data: {}
+        }
+
+        acc[identifier][:data][step_run.build_number] = {value: data["value"]}
+      end
+    end
   end
 
   def chart_data(metadata_id)
