fc352329b8a1031134ad263568db4bbe8c5e8a15
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2023-12-12 22:44:50 +0530

    fix data mig

--- db/data/20231211073727_add_review_failed_notification_settings.rb
+++ db/data/20231211073727_add_review_failed_notification_settings.rb
@@ -4,6 +4,8 @@ class AddReviewFailedNotificationSettings < ActiveRecord::Migration[7.0]
   def up
     ActiveRecord::Base.transaction do
       Train.all.where.not(notification_channel: nil).each do |train|
+        next if train.notification_settings.empty?
+
         review_failed_setting = train.notification_settings.find_by(kind: NotificationSetting.kinds[:review_failed])
         next if review_failed_setting.present?
 
