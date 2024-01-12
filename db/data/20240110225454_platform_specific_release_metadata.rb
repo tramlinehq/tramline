dd357a79e74d3cf87adb9a2b9d0877032e08881d
Akshay Gupta <kitallis@gmail.com>
2024-01-11 23:17:43 -0800

    lint

--- db/data/20240110225454_platform_specific_release_metadata.rb
+++ db/data/20240110225454_platform_specific_release_metadata.rb
@@ -4,8 +4,8 @@ class PlatformSpecificReleaseMetadata < ActiveRecord::Migration[7.0]
       runs = r.release_platform_runs.includes(:release_platform)
 
       if r.app.cross_platform?
-        android = runs.where(release_platform: { platform: :android }).sole
-        ios = runs.where(release_platform: { platform: :ios }).sole
+        android = runs.where(release_platform: {platform: :android}).sole
+        ios = runs.where(release_platform: {platform: :ios}).sole
         dup_release_metadata = r.release_metadata.dup
         r.release_metadata.update!(release_platform_run: android, promo_text: nil)
         dup_release_metadata.update!(release_platform_run: ios)
