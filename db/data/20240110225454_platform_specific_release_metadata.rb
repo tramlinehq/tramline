f18c8a1ae0308bc63a7c446e7c202c9f037d80fc
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2024-01-15 12:23:36 +0530

    fix data migration for hotfixes

--- db/data/20240110225454_platform_specific_release_metadata.rb
+++ db/data/20240110225454_platform_specific_release_metadata.rb
@@ -4,11 +4,16 @@ class PlatformSpecificReleaseMetadata < ActiveRecord::Migration[7.0]
       runs = r.release_platform_runs.includes(:release_platform)
 
       if r.app.cross_platform?
-        android = runs.where(release_platform: {platform: :android}).sole
-        ios = runs.where(release_platform: {platform: :ios}).sole
-        dup_release_metadata = r.release_metadata.dup
-        r.release_metadata.update!(release_platform_run: android, promo_text: nil)
-        dup_release_metadata.update!(release_platform_run: ios)
+        android = runs.where(release_platform: {platform: :android}).first
+        ios = runs.where(release_platform: {platform: :ios}).first
+        if android && ios
+          r.release_metadata.update!(release_platform_run: android, promo_text: nil)
+          r.release_metadata.dup.update!(release_platform_run: ios)
+        elsif android
+          r.release_metadata.update!(release_platform_run: android, promo_text: nil)
+        elsif ios
+          r.release_metadata.update!(release_platform_run: ios)
+        end
       else
         r.release_metadata.update!(release_platform_run: runs.sole)
       end
