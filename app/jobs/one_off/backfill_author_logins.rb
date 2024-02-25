7420721dbc8c6b5ca90369aeedbedaea8443d4a0
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2024-02-25 20:36:40 +0530

    one off task to backfill release commit authors

---
+++ app/jobs/one_off/backfill_author_logins.rb
@@ -0,0 +1,21 @@
+class OneOff::BackfillAuthorLogins < ApplicationJob
+  include Loggable
+  queue_as :low
+
+  def perform(release_id)
+    release = Release.find(release_id)
+    vcs_provider = release.train.vcs_provider
+    return unless vcs_provider.to_s == "github"
+
+    Commit.transaction do
+      release.all_commits.each do |commit|
+        next if commit.author_login.present?
+        commit_info = vcs_provider.installation.client.commit(vcs_provider.code_repository_name, commit.commit_hash)
+        commit.update! author_login: commit_info.author.login
+      rescue => e
+        Rails.logger.error(e)
+        next
+      end
+    end
+  end
+end
