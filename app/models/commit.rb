403227c86b82b6f65e56842af04b3f36351f91eb
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2024-02-26 12:29:22 +0530

    Fix the commits by team query again, final fix

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -46,7 +46,7 @@ class Commit < ApplicationRecord
 
     reorder("")
       .left_outer_joins(user: [memberships: :team])
-      .where(memberships: {organization_id: org.id})
+      .where("memberships.organization_id = ? OR memberships.organization_id IS NULL", org.id)
       .group("COALESCE(teams.name, '#{Accounts::Team::UNKNOWN_TEAM_NAME}')")
       .count("commits.id")
       .sort_by(&:last)
