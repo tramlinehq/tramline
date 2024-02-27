1cfcdb7f80a24c9f47e4613d773fc0665691ef79
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2024-02-27 19:42:33 +0530

    add contextual back buttons

--- app/components/v2/back_button_component.rb
+++ app/components/v2/back_button_component.rb
@@ -1,7 +1,8 @@
 class V2::BackButtonComponent < ViewComponent::Base
-  def initialize(path = nil)
+  def initialize(path: nil, to: nil)
     @path = path
     @link_data = (!path) ? history_nav : {}
+    @to = to
   end
 
   def history_nav
@@ -9,4 +10,9 @@ class V2::BackButtonComponent < ViewComponent::Base
   end
 
   attr_reader :path, :link_data
+
+  def tooltip_text
+    return "Go back" unless @to
+    "Go back to #{@to}"
+  end
 end
