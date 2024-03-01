1a8d22e0dfebce8890ced471024daf1d176da104
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2024-03-01 14:49:30 +0530

    fix dropdown width and checkmark margins

--- app/components/v2/dropdown_component.rb
+++ app/components/v2/dropdown_component.rb
@@ -55,7 +55,7 @@ class V2::DropdownComponent < V2::BaseComponent
       end
 
       ITEM_STYLE = "flex items-center rounded justify-between block px-4 py-1.5 hover:bg-main-100 dark:hover:bg-main-600 dark:hover:text-white text-sm"
-      SELECTED_CHECK_STYLE = "w-3 h-3 text-green-500"
+      SELECTED_CHECK_STYLE = "ml-4 w-3 h-3 text-green-500"
 
       def call
         if @link.present?
