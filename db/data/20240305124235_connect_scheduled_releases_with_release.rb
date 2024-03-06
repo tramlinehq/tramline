6e1cd58f63bf67ddf4097cc328b7df90909eb989
Akshay Gupta <kitallis@gmail.com>
2024-03-06 15:24:49 +0530

    Fix where clause

--- db/data/20240305124235_connect_scheduled_releases_with_release.rb
+++ db/data/20240305124235_connect_scheduled_releases_with_release.rb
@@ -2,7 +2,7 @@ class ConnectScheduledReleasesWithRelease < ActiveRecord::Migration[7.0]
   def up
     ActiveRecord::Base.transaction do
       # scan all automatic trains
-      Train.where.not(kickoff_at: nil, repeat_duration: nil).each do |train|
+      Train.where.not(kickoff_at: nil).where.not(repeat_duration: nil).each do |train|
         # check redundantly to be sure
         next unless train.automatic?
 
