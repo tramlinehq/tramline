32fe5087fc0341e1d9ec139f93560425852b4ac0
Akshay Gupta <kitallis@gmail.com>
2024-03-06 18:09:02 +0530

    Last 10 scheduled releases for data migration

--- db/data/20240305124235_connect_scheduled_releases_with_release.rb
+++ db/data/20240305124235_connect_scheduled_releases_with_release.rb
@@ -6,13 +6,13 @@ class ConnectScheduledReleasesWithRelease < ActiveRecord::Migration[7.0]
         # check redundantly to be sure
         next unless train.automatic?
 
-        # find last 4 scheduled releases
-        last_4 = train.scheduled_releases.where("scheduled_at < ?", Time.current.beginning_of_day).order(scheduled_at: :asc).last(4)
+        # find last 10 scheduled releases
+        last_10 = train.scheduled_releases.where("scheduled_at < ?", Time.current.beginning_of_day).order(scheduled_at: :asc).last(10)
 
         # skip if there are no scheduled releases
-        next if last_4.blank?
+        next if last_10.blank?
 
-        last_4.each do |scheduled_release|
+        last_10.each do |scheduled_release|
           # find a release within a 10 minute window around the scheduled release schedule time
           target_date = scheduled_release.scheduled_at
           release = train.releases.where("scheduled_at >= ? AND scheduled_at <= ?", target_date - 10.minutes, target_date + 10.minutes).first
