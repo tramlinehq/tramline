bcf5324cd3e057fb46c6e25a11c7097a066ec602
Akshay Gupta <kitallis@gmail.com>
2024-03-07 18:47:56 +0530

    rename confusing team variables

--- app/controllers/accounts/teams_controller.rb
+++ app/controllers/accounts/teams_controller.rb
@@ -4,8 +4,8 @@ class Accounts::TeamsController < SignedInApplicationController
       [1, "Settings", edit_accounts_organization_path(current_organization), "v2/cog.svg"],
       [2, "Team", accounts_organization_team_path(current_organization), "v2/user_cog.svg"]
     ]
-    @team = current_organization.users.includes(:invitations)
     @teams = current_organization.teams
-    @invited_team = current_organization.invites.includes(:sender).not_accepted
+    @users = current_organization.users.includes(:invitations)
+    @invited_users = current_organization.invites.includes(:sender).not_accepted
   end
 end
