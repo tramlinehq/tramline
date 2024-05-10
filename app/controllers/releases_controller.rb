7480c5c215f829b4a2031d4c08b929051571c873
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2024-05-11 00:12:24 +0530

    cleanup

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -147,35 +147,6 @@ class ReleasesController < SignedInApplicationController
 
   private
 
-  def set_tab_configuration
-    @tab_configuration =
-      {
-        "kickoff" => [
-          [1, "Overview", overview_release_path(@release), "v2/gauge.svg"],
-          [2, "Changeset tracking", change_queue_release_path(@release), "v2/list_end.svg"]
-        ],
-
-        "stability" => [
-          [2, "Internal builds", root_path, "v2/drill.svg"],
-          [2, "Regression testing", root_path, "v2/tablet_smartphone.svg"],
-          [3, "Release candidate", root_path, "v2/gallery_horizontal_end.svg"],
-          [4, "Beta soak", root_path, "v2/alarm_clock.svg"]
-        ],
-
-        "metadata" => [
-          [2, "Notes", root_path, "v2/text.svg"],
-          [2, "Screenshots", root_path, "v2/wand.svg"]
-        ],
-
-        "release" => [
-          [2, "Approvals", root_path, "v2/list_checks.svg"],
-          [2, "App submission", store_submissions_release_path(@release), "v2/mail.svg"],
-          [3, "Store review", root_path, "v2/mail_search.svg"],
-          [4, "Rollout to users", root_path, "v2/rocket.svg"]
-        ]
-      }
-  end
-
   def post_release_params
     params.require(:release).permit(:force_finalize)
   end
