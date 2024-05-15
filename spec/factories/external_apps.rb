b0d0e02c1a3d8fd56d7bc1defaf4f98c33243b8e
Akshay Gupta <kitallis@gmail.com>
2024-05-15 13:49:33 +0530

    fix intermittent spec failure

--- spec/factories/external_apps.rb
+++ spec/factories/external_apps.rb
@@ -80,7 +80,7 @@ ANDROID_CHANNEL_DATA = [
         "localizations" => [
           {
             "text" => "• Update README.md\n• new RC build\n• new and improved RC",
-            "language" => "en-GB"
+            "language" => "en-AU"
           }
         ],
         "user_fraction" => nil,
