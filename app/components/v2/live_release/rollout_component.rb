ecbfb0620214455177380f88d3cd209940a6034d
Akshay Gupta <kitallis@gmail.com>
2024-05-17 17:06:07 +0530

    add activity timeline to rollout card

--- app/components/v2/live_release/rollout_component.rb
+++ app/components/v2/live_release/rollout_component.rb
@@ -8,4 +8,31 @@ class V2::LiveRelease::RolloutComponent < V2::BaseComponent
   def monitoring_size
     release_platform_run.app.cross_platform? ? :compact : :default
   end
+
+  def events
+    [{
+      timestamp: time_format(1.day.ago, with_year: false),
+      title: "Rollout increase",
+      description: "The staged rollout for this release has been increased to 50%",
+      type: :success
+    },
+     {
+       timestamp: time_format(2.day.ago, with_year: false),
+       title: "Rollout increase",
+       description: "The staged rollout for this release has been increased to 20%",
+       type: :success
+     },
+     {
+       timestamp: time_format(3.day.ago, with_year: false),
+       title: "Rollout increase",
+       description: "The staged rollout for this release has been increased to 10%",
+       type: :success
+     },
+     {
+       timestamp: time_format(4.day.ago, with_year: false),
+       title: "Rollout increase",
+       description: "The staged rollout for this release has been increased to 1%",
+       type: :success
+     }]
+  end
 end
