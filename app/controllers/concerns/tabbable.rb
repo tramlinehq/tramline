7cd310553d23c7f00829715f7672e95af2dcf3c8
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2024-05-22 17:37:49 +0530

    remove store review tab

--- app/controllers/concerns/tabbable.rb
+++ app/controllers/concerns/tabbable.rb
@@ -7,22 +7,21 @@ module Tabbable
       ],
 
       "stability" => [
-        [2, "Internal builds", internal_builds_release_path(@release), "v2/drill.svg"],
+        [1, "Internal builds", internal_builds_release_path(@release), "v2/drill.svg"],
         [2, "Regression testing", regression_testing_release_path(@release), "v2/tablet_smartphone.svg"],
         [3, "Release candidate", root_path, "v2/gallery_horizontal_end.svg"],
         [4, "Soak period", soak_release_path(@release), "v2/alarm_clock.svg"]
       ],
 
       "metadata" => [
-        [2, "Notes", release_metadata_edit_path(@release), "v2/text.svg"],
+        [1, "Notes", release_metadata_edit_path(@release), "v2/text.svg"],
         [2, "Screenshots", root_path, "v2/wand.svg"]
       ],
 
       "release" => [
-        [2, "Approvals", root_path, "v2/list_checks.svg"],
+        [1, "Approvals", root_path, "v2/list_checks.svg"],
         [2, "App submission", store_submissions_release_path(@release), "v2/mail.svg"],
-        [3, "Store review", root_path, "v2/mail_search.svg"],
-        [4, "Rollout to users", release_staged_rollout_edit_path(@release), "v2/rocket.svg"]
+        [3, "Rollout to users", release_staged_rollout_edit_path(@release), "v2/rocket.svg"]
       ]
     }
   end
