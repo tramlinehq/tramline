f84339d95636ecdbb0ed31c39c67c85c4c326eb6
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2024-05-23 14:33:45 +0530

    Persist team stats for release summary separately in cache
    - use persisted release summary team stats in train devops report

--- app/components/v2/live_release/regression_testing_component.rb
+++ app/components/v2/live_release/regression_testing_component.rb
@@ -10,20 +10,19 @@ class V2::LiveRelease::RegressionTestingComponent < V2::BaseComponent
 
   def events
     [{
-       timestamp: time_format(1.day.ago, with_year: false),
-       description: "Build #239 was rejected by Derek O' Brien",
-       type: :error
-     },
-     {
-       timestamp: time_format(2.days.ago, with_year: false),
-       description: "Build #238 was approved by Akhil Vaidya",
-       type: :success
-     },
-     {
-       timestamp: time_format(3.days.ago, with_year: false),
-       description: "Build #237 was approved by Sagar Neeraj",
-       type: :success
-     },]
-
+      timestamp: time_format(1.day.ago, with_year: false),
+      description: "Build #239 was rejected by Derek O' Brien",
+      type: :error
+    },
+      {
+        timestamp: time_format(2.days.ago, with_year: false),
+        description: "Build #238 was approved by Akhil Vaidya",
+        type: :success
+      },
+      {
+        timestamp: time_format(3.days.ago, with_year: false),
+        description: "Build #237 was approved by Sagar Neeraj",
+        type: :success
+      }]
   end
 end
