3beda979ee87a67f044433c512afd4b85584f3c9
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2024-06-25 17:31:23 -0700

    Fix next to next version calculation

--- app/models/concerns/versionable.rb
+++ app/models/concerns/versionable.rb
@@ -3,12 +3,12 @@ module Versionable
 
   def next_version(major_only: false, patch_only: false)
     raise ArgumentError, "both major and patch cannot be true" if major_only && patch_only
-    patch_only = true if patch_version_bump_only && !major_only
     version = version_current.to_semverish
     version_current.ver_bump(bump_term(version, major_only:, patch_only:), strategy: versioning_strategy)
   end
 
   def next_to_next_version(major_only: false, patch_only: false)
+    version = version_current.to_semverish
     next_version(major_only: major_only, patch_only: patch_only)
       .ver_bump(bump_term(version, major_only:, patch_only:), strategy: versioning_strategy)
   end
@@ -16,6 +16,7 @@ module Versionable
   private
 
   def bump_term(version, major_only: false, patch_only: false)
+    patch_only = true if patch_version_bump_only && !major_only
     if patch_only && version.proper?
       :patch
     else
