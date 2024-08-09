01dbf96fc3a621d695db0243cd1d884496201210
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2024-08-09 11:11:35 +0530

    fix production release factory

--- spec/factories/production_releases.rb
+++ spec/factories/production_releases.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :production_release do
     release_platform_run { association :release_platform_run }
     build { association :build }
+    status { "inflight" }
   end
 end
