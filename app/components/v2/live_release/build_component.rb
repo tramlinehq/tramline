8fd3235183c5acc540279974afea0e996ef7fc53
Akshay Gupta <kitallis@gmail.com>
2024-08-19 18:47:28 +0530

    wrap up build component

--- app/components/v2/live_release/build_component.rb
+++ app/components/v2/live_release/build_component.rb
@@ -3,16 +3,17 @@
 class V2::LiveRelease::BuildComponent < V2::BaseComponent
   include Memery
 
-  def initialize(build, show_number: false, show_build_only: false, show_metadata: true, show_ci: true, show_activity: true, show_commit: true)
+  def initialize(build, show_number: true, show_metadata: true, show_ci: true, show_activity: true, show_commit: true, show_compact_metadata: false)
     @build = build
     @show_number = show_number
     @show_metadata = show_metadata
     @show_ci = show_ci
     @show_activity = show_activity
     @show_commit = show_commit
+    @show_compact_metadata = show_compact_metadata
   end
 
-  attr_reader :build, :previous_build, :show_build_only, :show_number, :show_metadata, :show_ci, :show_activity, :show_commit
+  attr_reader :build, :previous_build, :show_number, :show_metadata, :show_ci, :show_activity, :show_commit, :show_compact_metadata
   delegate :release_platform_run, :commit, :version_name, :artifact, :workflow_run, to: :build
   delegate :external_url, :external_number, to: :workflow_run
 
@@ -28,23 +29,8 @@ class V2::LiveRelease::BuildComponent < V2::BaseComponent
     "Build ##{external_number}"
   end
 
-  # TODO: [V2] fix this
-  def ci_link
-    external_url
-  end
-
   def build_logo
-    "integrations/logo_#{release_platform_run.release.train.ci_cd_provider}.png"
-  end
-
-  def commits_since_last_release
-    return unless previous_build
-    release_platform_run.all_commits.between_commits(previous_build&.commit, commit)
-  end
-
-  def diff_between
-    return unless previous_build
-    "#{previous_build.display_name} → #{build.display_name}"
+    "integrations/logo_#{build.ci_cd_provider}.png"
   end
 
   def last_activity_at
