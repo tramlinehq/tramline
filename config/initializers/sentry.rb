830f0be443be0d072962cdaa3343269463664a95
Akshay Gupta <kitallis@gmail.com>
2024-08-28 01:09:29 +0530

    adjust sentry sampling rate

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,20 +1,20 @@
 Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-  config.enabled_environments = %w[production]
-  # config.traces_sample_rate = 0.5
-  config.traces_sampler = lambda do |sampling_context|
-    return 0.0 if ENV["RAILS_PIPELINE_ENV"].eql?("staging")
-    transaction_context = sampling_context[:transaction_context]
-    op = transaction_context[:op]
-    case op
-    when /request/ # web requests
-      0.2
-    when /sidekiq/i # background jobs
-      0.05
-    else
-      0.0
-    end
-  end
+  config.enabled_environments = %w[production development]
+  config.traces_sample_rate = (ENV["RAILS_PIPELINE_ENV"].eql?("staging") ? 0.2 : 0.5)
+  # config.traces_sampler = lambda do |sampling_context|
+  #   return 0.0 if ENV["RAILS_PIPELINE_ENV"].eql?("staging")
+  #   transaction_context = sampling_context[:transaction_context]
+  #   op = transaction_context[:op]
+  #   case op
+  #   when /request/ # web requests
+  #     0.2
+  #   when /sidekiq/i # background jobs
+  #     0.05
+  #   else
+  #     0.0
+  #   end
+  # end
   config.send_default_pii = true
   config.logger = Logger.new($stdout)
   config.logger.level = Logger::WARN
