33607f76378c6eef0296bb78c9864321bc58f087
Akshay Gupta <kitallis@gmail.com>
2024-10-06 23:32:54 +0530

    Fix

--- app/models/play_store_submission.rb
+++ app/models/play_store_submission.rb
@@ -260,7 +260,7 @@ class PlayStoreSubmission < StoreSubmission
 
   def on_prepare!
     event_stamp!(reason: :prepared, kind: :notice, data: stamp_data)
-    config = conf.rollout_config.stages.presence || []
+    config = conf.rollout_stages.presence || []
     create_play_store_rollout!(release_platform_run:, config:, is_staged_rollout: staged_rollout?)
     play_store_rollout.start_release!(retry_on_review_fail: internal_channel?) if auto_rollout?
   end
