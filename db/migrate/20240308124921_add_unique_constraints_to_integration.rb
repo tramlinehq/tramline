35aae5e382921dbfc9ed5ed5c3df18188c4951f9
Akshay Gupta <kitallis@gmail.com>
2024-10-10 14:54:15 +0530

    remove unecessary change in migration

--- db/migrate/20240308124921_add_unique_constraints_to_integration.rb
+++ db/migrate/20240308124921_add_unique_constraints_to_integration.rb
@@ -2,10 +2,6 @@ class AddUniqueConstraintsToIntegration < ActiveRecord::Migration[7.0]
   disable_ddl_transaction!
 
   def change
-    add_column :integrations, :providable_id, :uuid
-    add_column :integrations, :providable_type, :string
-    add_index :integrations, [:providable_type, :providable_id], unique: true
-
     add_index :integrations, [:app_id, :category, :providable_type, :status], unique: true, where: "status = 'connected'", name: "unique_connected_integration_category", algorithm: :concurrently
   end
 end
