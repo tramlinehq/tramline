3b489a58435c8751e3db3024572bdd22d4f3dae8
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2024-11-26 20:56:24 +0530

    Allow no submissions to be configured for internal release step
    - this will build an internal build and share it only via the notification

--- app/libs/notifiers/slack/renderers/internal_release_finished.rb
+++ app/libs/notifiers/slack/renderers/internal_release_finished.rb
@@ -2,6 +2,12 @@ module Notifiers
   module Slack
     class Renderers::InternalReleaseFinished < Renderers::Base
       TEMPLATE_FILE = "internal_release_finished.json.erb".freeze
+
+      def main_text
+        text = ":sparkles: The internal build step finished for *#{@release_version} (#{@build_number})* for commit <#{@commit_url}|#{@commit_sha}>."
+        text += " The build has been sent to #{@submission_channels}." if @submission_channels.present?
+        text
+      end
     end
   end
 end
