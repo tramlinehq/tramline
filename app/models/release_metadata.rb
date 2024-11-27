f13b0c212ce711be27642baccdcc5dbc45aec790
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2024-11-27 15:27:32 +0530

    Allow platform-specific max notes check for release notes
    - Fix play store submission to log unknown failure properly in the
    passport message and notifications
    - Sanitize notes to normalize new lines across different platforms

--- app/models/release_metadata.rb
+++ app/models/release_metadata.rb
@@ -20,7 +20,9 @@ class ReleaseMetadata < ApplicationRecord
   belongs_to :release, inverse_of: :release_metadata
   belongs_to :release_platform_run, inverse_of: :release_metadata, optional: true
 
-  NOTES_MAX_LENGTH = 4000
+  NOTES_MAX_LENGTH = 4000 # TODO [V2]: remove this and use the platform-specific notes max length
+  IOS_NOTES_MAX_LENGTH = 4000
+  ANDROID_NOTES_MAX_LENGTH = 500
   # NOTE: Refer to https://www.regular-expressions.info/unicode.html for supporting more unicode characters
   PLAINTEXT_REGEX = /\A[~₹!@#$%^&*()_+\-=\[\]{};':"\\|`,.\/?\s\p{Alnum}\p{P}\p{Zs}\p{Emoji_Presentation}\p{M}\p{N}]+\z/
   DEFAULT_LOCALES = ["en-US", "en-GB", "hi-IN", "en-IN", "id"]
@@ -29,15 +31,32 @@ class ReleaseMetadata < ApplicationRecord
   DEFAULT_RELEASE_NOTES = "The latest version contains bug fixes and performance improvements."
 
   validates :release_notes,
-    format: {with: PLAINTEXT_REGEX, message: :no_special_characters, multiline: true},
-    length: {maximum: NOTES_MAX_LENGTH}
+    format: {with: PLAINTEXT_REGEX, message: :no_special_characters, multiline: true}
   validates :promo_text,
     format: {with: PLAINTEXT_REGEX, message: :no_special_characters, allow_blank: true, multiline: true},
     length: {maximum: 170}
   validates :locale, uniqueness: {scope: :release_platform_run_id}
+  validate :notes_length
+
+  # NOTE: strip and normalize line endings across various OSes
+  normalizes :release_notes, with: ->(notes) { notes.strip.gsub("\r\n?", "\n") }
 
   def self.find_by_id_and_language(id, language, platform)
     locale_tag = AppStores::Localizable.supported_locale_tag(language, platform)
     ReleaseMetadata.find_by(id: id, locale: locale_tag)
+  end
+
+  private
+
+  def notes_length
+    errors.add(:release_notes, :too_long, max_length: notes_max_length) if release_notes.length > notes_max_length
+  end
+
+  def notes_max_length
+    case release_platform_run.platform
+    when "android" then ANDROID_NOTES_MAX_LENGTH
+    when "ios" then IOS_NOTES_MAX_LENGTH
+    else raise ArgumentError, "Invalid platform"
+    end
   end
 end
