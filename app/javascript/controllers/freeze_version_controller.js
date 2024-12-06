baa348da414feb3652e688610746cbb39f98e984
gitstart_bot <gitstart_bot@users.noreply.github.com>
2024-12-06 12:47:22 +0000

    Allow fixed build version for trains (non-production release trains)

--- app/javascript/controllers/freeze_version_controller.js
+++ app/javascript/controllers/freeze_version_controller.js
@@ -2,16 +2,30 @@ import {Controller} from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["freezeReleaseVersion", "patchVersionBumpOnly"]
+  static outlets = ["domain--version-name-help"];
 
   initialize() {
     this.updatePatchVersionBumpState()
+    this.updateNextReleaseVersion()
   }
 
   change() {
     this.updatePatchVersionBumpState()
+    this.updateNextReleaseVersion()
   }
 
   updatePatchVersionBumpState() {
     this.patchVersionBumpOnlyTarget.disabled = this.freezeReleaseVersionTarget.checked;
+  }
+
+  updateNextReleaseVersion() {
+    if (this.hasDomainVersionNameHelpOutlet) {
+      const versionNameHelpController = this.domainVersionNameHelpOutlet;
+      if (this.freezeReleaseVersionTarget.checked) {
+        versionNameHelpController.nextVersionTarget.innerHTML = versionNameHelpController.__versionString();
+      } else {
+        versionNameHelpController.nextVersionTarget.innerHTML = versionNameHelpController.__nextReleaseVersion();
+      }
+    }
   }
 }
