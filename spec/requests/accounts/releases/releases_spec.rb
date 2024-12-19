b922f463bcb5e3afc9eed9f6421c64a28faf2e08
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2024-12-19 18:23:04 +0530

    fix a pending spec

--- spec/requests/accounts/releases/releases_spec.rb
+++ spec/requests/accounts/releases/releases_spec.rb
@@ -1,18 +1,18 @@
 require "rails_helper"
 
 describe "Accounts::Releases::Releases" do
-  describe "GET /show" do
-    let(:release) { create(:release, :created) }
+  describe "GET /overview" do
+    let(:train) { create(:train) }
+    let(:release_platform) { create(:release_platform, train:) }
+    let(:release) { create(:release, :created, :with_no_platform_runs) }
     let(:organization) { release.train.app.organization }
     let(:user) { create(:user, :with_email_authentication, :as_developer, member_organization: organization) }
-    let(:release_platform) { create(:release_platform, train: release.train) }
 
     it "returns success code" do
-      pending "move this to the new routes"
       create(:release_platform_run, :created, release:, release_platform:)
 
       sign_in user.email_authentication
-      get release_path(release.id)
+      get overview_release_path(release.id)
 
       expect(response).to have_http_status(:ok)
     end
