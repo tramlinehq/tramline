03d96ea8593abda166cedd942d2b273a067e67cc
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2024-12-23 18:57:46 +0530

    Fix release end notification

--- app/libs/notifiers/slack/renderers/release_ended.rb
+++ app/libs/notifiers/slack/renderers/release_ended.rb
@@ -1,14 +1,14 @@
 module Notifiers
   module Slack
-    include ActionView::Helpers::DateHelper
-
     class Renderers::ReleaseEnded < Renderers::Base
-      TEMPLATE_FILE = "release_ended.json.erb".freeze
-    end
+      include ActionView::Helpers::DateHelper
 
-    def total_run_time
-      return "N/A" if @release_completed_at.blank? || @release_started_at.blank?
-      distance_of_time_in_words(@release_started_at, @release_completed_at)
+      TEMPLATE_FILE = "release_ended.json.erb".freeze
+
+      def total_run_time
+        return "N/A" if @release_completed_at.blank? || @release_started_at.blank?
+        distance_of_time_in_words(@release_started_at, @release_completed_at)
+      end
     end
   end
 end
