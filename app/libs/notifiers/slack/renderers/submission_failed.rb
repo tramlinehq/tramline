7d6dd01cadf1018cd83f817729b789e2519a536a
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2025-01-04 12:36:23 +0530

    fix submission failed notif

--- app/libs/notifiers/slack/renderers/submission_failed.rb
+++ app/libs/notifiers/slack/renderers/submission_failed.rb
@@ -7,7 +7,7 @@ module Notifiers
         text = ":octagonal_sign: The submission to *#{@submission_channel}* failed for *#{@release_version} (#{@build_number})* for <#{@commit_url}|#{@commit_sha}>"
         text += " and requires a manual intervention" if @submission_requires_manual_action
         text += ". Please check the release page for more details. \n\n *Error* \n ```#{@submission_failure_reason}```"
-        text
+        safe_string(text)
       end
     end
   end
