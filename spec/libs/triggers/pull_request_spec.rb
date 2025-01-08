196ad82d9c39245db38409deea504bf2713174a8
Nivedita Priyadarshini <nid.mishra7@gmail.com>
2025-01-09 00:24:29 +0530

    Enable auto merge for PRs when when the first merge attampt fails
    - This will ensure that we try to merge PRs for repositories which do not have the
    auto merge feature available

--- spec/libs/triggers/pull_request_spec.rb
+++ spec/libs/triggers/pull_request_spec.rb
@@ -26,7 +26,7 @@ describe Triggers::PullRequest do
     let(:merge_payload) { JSON.parse(File.read("spec/fixtures/github/merge_pull_request.json")).with_indifferent_access }
 
     it "creates a PR for the release" do
-      allow(repo_integration).to receive_messages(create_pr!: create_payload, enable_auto_merge: true)
+      allow(repo_integration).to receive_messages(create_pr!: create_payload, merge_pr!: merge_payload, enable_auto_merge: true)
 
       result = described_class.create_and_merge!(
         release: release,
@@ -44,7 +44,7 @@ describe Triggers::PullRequest do
     end
 
     it "creates a patch PR for the commit" do
-      allow(repo_integration).to receive_messages(cherry_pick_pr: create_payload, enable_auto_merge: true)
+      allow(repo_integration).to receive_messages(cherry_pick_pr: create_payload, merge_pr!: merge_payload, enable_auto_merge: true)
       commit = create(:commit, release:)
       patch_branch = "patch-main-#{commit.commit_hash}"
 
@@ -64,8 +64,25 @@ describe Triggers::PullRequest do
       expect(commit.reload.pull_request).to be_present
     end
 
-    it "does not merge the PR if auto merge is enabled" do
+    it "merges the PR" do
+      allow(repo_integration).to receive_messages(create_pr!: create_payload, merge_pr!: merge_payload, enable_auto_merge: true)
+
+      result = described_class.create_and_merge!(
+        release: release,
+        new_pull_request: release.pull_requests.post_release.open.build,
+        to_branch_ref: working_branch,
+        from_branch_ref: release_branch,
+        title: pr_title,
+        description: pr_description
+      )
+      expect(repo_integration).to have_received(:merge_pr!).with(repo_name, result.value!.number)
+      expect(result.ok?).to be(true)
+      expect(release.reload.pull_requests.size).to eq(1)
+    end
+
+    it "enables auto merge if merge fails" do
       allow(repo_integration).to receive_messages(cherry_pick_pr: create_payload, enable_auto_merge: true)
+      allow(repo_integration).to receive(:merge_pr!).and_raise(Installations::Error.new("Failed to merge the Pull Request", reason: :pull_request_not_mergeable))
       commit = create(:commit, release:)
       patch_branch = "patch-main-#{commit.commit_hash}"
 
@@ -83,6 +100,7 @@ describe Triggers::PullRequest do
       created_pr = commit.reload.pull_request
 
       expect(repo_integration).to have_received(:cherry_pick_pr).with(repo_name, working_branch, commit.commit_hash, patch_branch, pr_title, pr_description, GithubIntegration::PR_TRANSFORMATIONS)
+      expect(repo_integration).to have_received(:merge_pr!).with(repo_name, created_pr.number)
       expect(repo_integration).to have_received(:enable_auto_merge).with(app.config.code_repo_namespace, app.config.code_repo_name_only, created_pr.number)
       expect(result.ok?).to be(true)
       expect(created_pr).to be_present
@@ -91,7 +109,6 @@ describe Triggers::PullRequest do
 
     it "does not create PR if the PR does not have a diff to create" do
       allow(repo_integration).to receive(:create_pr!).and_raise(no_diff_error)
-      allow(repo_integration).to receive(:enable_auto_merge)
 
       result = described_class.create_and_merge!(
         release: release,
@@ -105,14 +122,12 @@ describe Triggers::PullRequest do
       namespaced_release_branch = "#{release.train.app.config.code_repo_namespace}:#{release_branch}"
 
       expect(repo_integration).to have_received(:create_pr!).with(repo_name, working_branch, namespaced_release_branch, pr_title, pr_description, GithubIntegration::PR_TRANSFORMATIONS)
-      expect(repo_integration).not_to have_received(:enable_auto_merge)
       expect(result.ok?).to be(true)
       expect(release.reload.pull_requests.size).to eq(0)
     end
 
     it "returns an unsuccessful result if the PR does not have a diff to create and allow without diff is false" do
       allow(repo_integration).to receive(:create_pr!).and_raise(no_diff_error)
-      allow(repo_integration).to receive(:enable_auto_merge)
 
       result = described_class.create_and_merge!(
         release: release,
@@ -126,7 +141,6 @@ describe Triggers::PullRequest do
       namespaced_release_branch = "#{release.train.app.config.code_repo_namespace}:#{release_branch}"
 
       expect(repo_integration).to have_received(:create_pr!).with(repo_name, working_branch, namespaced_release_branch, pr_title, pr_description, GithubIntegration::PR_TRANSFORMATIONS)
-      expect(repo_integration).not_to have_received(:enable_auto_merge)
       expect(result.ok?).to be(false)
       expect(release.reload.pull_requests.size).to eq(0)
     end
