2206535e7fc788e8a6bf6bfc41c0202d2f322f82
Akshay Gupta <kitallis@gmail.com>
2025-01-30 13:06:27 +0530

    add newlines at EOF

--- db/migrate/20250128130924_add_search_indexes_to_pull_requests_and_commits.rb
+++ db/migrate/20250128130924_add_search_indexes_to_pull_requests_and_commits.rb
@@ -3,41 +3,41 @@ class AddSearchIndexesToPullRequestsAndCommits < ActiveRecord::Migration[7.0]
 
   def up
     enable_extension 'pg_trgm'
-    
+
     add_column :pull_requests, :search_vector, :tsvector
     add_column :commits, :search_vector, :tsvector
-    
+
     # Add GIN indexes for text search fields
-    add_index :pull_requests, :title, 
-      using: :gin, 
-      opclass: :gin_trgm_ops, 
-      algorithm: :concurrently
-      
-    add_index :pull_requests, :body, 
-      using: :gin, 
-      opclass: :gin_trgm_ops, 
-      algorithm: :concurrently
-      
-    add_index :pull_requests, :search_vector, 
-      using: :gin, 
-      algorithm: :concurrently
-    
+    add_index :pull_requests, :title,
+              using: :gin,
+              opclass: :gin_trgm_ops,
+              algorithm: :concurrently
+
+    add_index :pull_requests, :body,
+              using: :gin,
+              opclass: :gin_trgm_ops,
+              algorithm: :concurrently
+
+    add_index :pull_requests, :search_vector,
+              using: :gin,
+              algorithm: :concurrently
+
     # Commit indexes
-    add_index :commits, :message, 
-      using: :gin, 
-      opclass: :gin_trgm_ops, 
-      algorithm: :concurrently
-      
-    add_index :commits, :search_vector, 
-      using: :gin, 
-      algorithm: :concurrently
-    
+    add_index :commits, :message,
+              using: :gin,
+              opclass: :gin_trgm_ops,
+              algorithm: :concurrently
+
+    add_index :commits, :search_vector,
+              using: :gin,
+              algorithm: :concurrently
+
     # Generate initial search vectors
     PullRequest.find_each do |pr|
       search_text = [pr.title, pr.body, pr.number.to_s].compact.join(' ')
       pr.update_column :search_vector, PullRequest.generate_search_vector(search_text)
     end
-    
+
     Commit.find_each do |commit|
       commit.update_column :search_vector, Commit.generate_search_vector(commit.message)
     end
@@ -49,8 +49,8 @@ class AddSearchIndexesToPullRequestsAndCommits < ActiveRecord::Migration[7.0]
     remove_index :pull_requests, :search_vector, algorithm: :concurrently
     remove_index :commits, :message, algorithm: :concurrently
     remove_index :commits, :search_vector, algorithm: :concurrently
-    
+
     remove_column :pull_requests, :search_vector
     remove_column :commits, :search_vector
   end
-end 
+end
