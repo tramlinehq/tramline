fc91945be1369d341820757eda34c42c1850bb2e
Akshay Gupta <kitallis@gmail.com>
2025-02-05 17:56:58 +0530

    update the sidebar

--- app/components/vertical_tab_component.rb
+++ app/components/vertical_tab_component.rb
@@ -3,19 +3,20 @@
 class VerticalTabComponent < BaseComponent
   renders_many :tabs
 
-  TAB_STYLE = "inline-flex items-center justify-start py-4 pr-4 pl-0.5 border-b-2 border-transparent rounded-t-lg hover:text-main-600 hover:border-main-300 dark:hover:text-main-300 group"
-  SELECTED_TAB_STYLE = "inline-flex items-center justify-start py-4 pr-4 pl-0.5 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500 group"
+  TAB_STYLE = "flex flex-row items-center justify-between w-full rounded hover:text-main-900 hover:bg-main-100 px-2"
+  SELECTED_TAB_STYLE = "flex flex-row items-center justify-between w-full rounded hover:text-main-900 hover:bg-main-100 active text-main bg-main-100 border-l-2 border-main-400 px-2"
 
-  def initialize(turbo_frame:, tab_config: [], error_resource: nil)
+  def initialize(turbo_frame:, sidebar_header:, tab_config: [], error_resource: nil)
     raise ArgumentError, "tab_config must be an array" unless tab_config.is_a?(Array)
     raise ArgumentError, "tab_config must be an array of arrays" unless tab_config.all? { _1.length == 4 }
 
     @tab_config = tab_config
     @turbo_frame = turbo_frame
     @error_resource = error_resource
+    @sidebar_header = sidebar_header
   end
 
-  attr_reader :tab_config, :error_resource
+  attr_reader :tab_config, :error_resource, :sidebar_header
 
   def frame = @turbo_frame
 
