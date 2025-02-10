dbbb46873272634d4a311def35daf4141153ddae
Samrat Man Singh <samratmansingh@gmail.com>
2025-02-10 09:06:21 +0100

    Lint fixes

--- app/components/search_bar_component.rb
+++ app/components/search_bar_component.rb
@@ -9,20 +9,20 @@ class SearchBarComponent < BaseComponent
       placeholder: placeholder.presence || "Search",
       class: "form-input inline-flex",
       autocomplete: "off",
-      data: { search_form_target: "searchInput", action: "input->search-form#search" }
+      data: {search_form_target: "searchInput", action: "input->search-form#search"}
     }
   }
 
   renders_one :clear_search, -> {
     link_to "clear search",
-            "#",
-            title: "clear search",
-            class: "inline-flex underline relative hover:cursor-pointer text-xs top-2 right-0 ml-1",
-            data: { action: "search-form#clear" }
+      "#",
+      title: "clear search",
+      class: "inline-flex underline relative hover:cursor-pointer text-xs top-2 right-0 ml-1",
+      data: {action: "search-form#clear"}
   }
 
   renders_one :search_form, ->(url) {
-    form_with url: url, method: :get, data: { search_form_target: "form", turbo_frame: FRAME, turbo_action: "advance" } do |form|
+    form_with url: url, method: :get, data: {search_form_target: "form", turbo_frame: FRAME, turbo_action: "advance"} do |form|
       form.hidden_field QUERY_FIELD.to_sym
     end
   }
