8018db1707d608688ba5f1f5ce8dc68e630f4a06
NileshGR <627281+nileshgr@users.noreply.github.com>
2025-02-11 19:59:43 +0530

    Workflow parameters component

---
+++ app/components/workflow_parameters_component.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class WorkflowParametersComponent < ViewComponent::Base
+  attr_reader :form, :parameter_name_label, :parameter_value_label
+
+  def initialize(form, parameter_name_label: "Parameter Name", parameter_value_label: "Parameter Value", add_button: true, trash_button: true)
+    @form = form
+    @parameter_name_label = parameter_name_label
+    @parameter_value_label = parameter_value_label
+    @add_button = add_button
+    @trash_button = trash_button
+  end
+
+  def add_button? = @add_button
+end
