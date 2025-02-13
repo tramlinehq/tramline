8ba1d689026e394f85517b45c677d5ef6a093742
Samrat Man Singh <samratmansingh@gmail.com>
2025-02-13 14:20:17 +0100

    Align notice in search bar

--- app/components/search_bar_component.rb
+++ app/components/search_bar_component.rb
@@ -4,26 +4,19 @@ class SearchBarComponent < BaseComponent
   FRAME = "search_bar_and_results"
   QUERY_FIELD = "search_pattern"
 
-  renders_one :text_field, ->(name, value, placeholder) {
-    text_field_tag name, value, {
-      placeholder: placeholder.presence || "Search",
-      class: "form-input inline-flex w-full",
-      autocomplete: "off",
-      data: {search_form_target: "searchInput", action: "input->search-form#search"}
-    }
-  }
-
-  renders_one :clear_search, -> {
-    link_to "clear search",
-      "#",
-      title: "clear search",
-      class: "inline-flex underline relative hover:cursor-pointer text-xs top-2 right-0 ml-1",
-      data: {action: "search-form#clear"}
-  }
+  renders_one :notice
 
   renders_one :search_form, ->(url) {
     form_with url: url, method: :get, data: {search_form_target: "form", turbo_frame: FRAME, turbo_action: "advance"} do |form|
       form.hidden_field QUERY_FIELD.to_sym
     end
   }
+
+  def initialize(search_name:, search_value:, search_placeholder:)
+    @search_name = search_name
+    @search_value = search_value
+    @search_placeholder = search_placeholder
+  end
+
+  attr_reader :search_name, :search_value, :search_placeholder
 end
