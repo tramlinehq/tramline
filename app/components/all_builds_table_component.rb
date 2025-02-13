00808d5e005648df8e7c9df4ccae748a25da7e22
Samrat Man Singh <samratmansingh@gmail.com>
2025-02-13 15:53:16 +0100

    Design build search results page

--- app/components/all_builds_table_component.rb
+++ app/components/all_builds_table_component.rb
@@ -1,7 +1,8 @@
-class AllBuildsTableComponent < ViewComponent::Base
+class AllBuildsTableComponent < BaseComponent
   include Pagy::Frontend
-  include ApplicationHelper
-  include LinkHelper
+  include Memery
+
+  renders_one :filter
 
   def initialize(builds:, paginator:, query_params:)
     @builds = builds
@@ -13,66 +14,19 @@ class AllBuildsTableComponent < ViewComponent::Base
 
   attr_reader :builds, :paginator
 
-  def sort_link(column:, label:)
-    if column == @sort_column
-      link_to(path(column, next_direction), data: turbo_data) do
-        concat tag.span(label)
-        concat sort_indicator
-      end
-    else
-      link_to(path(column, "asc"), data: turbo_data) do
-        concat tag.span(label)
-        concat sort_indicator
-      end
-    end
-  end
-
-  def sort_indicator
-    image_tag("sort_indicator.svg", class: "inline-flex mx-2 align-baseline", width: 8)
-  end
-
   def release_status(build)
-    ReleasePresenter::RELEASE_STATUS.fetch(build.release_status.to_sym) => {text:, status:}
-    status_badge(text, status)
-  end
-
-  def release_platform(build)
-    I18n.t("activerecord.values.release_platform.platform.#{build.platform}")
-  end
-
-  def external_release_status?
-    builds.first.external_release_status.present?
-  end
-
-  def external_release_status(build)
-    status_badge(build.external_release_status.titleize.humanize, %w[mx-1], :routine)
+    helpers.status_picker(ReleasePresenter::RELEASE_STATUS, build.release_status)
   end
 
   def submissions(build)
     tag.div do
       if build.submissions.blank?
-        concat "–"
+        concat NOT_AVAILABLE
       else
         build.submissions.collect do |submission|
           concat tag.div submission.submission_info
         end
       end
     end
-  end
-
-  renders_one :filter
-
-  private
-
-  def next_direction
-    (@sort_direction == "asc") ? "desc" : "asc"
-  end
-
-  def path(column, direction)
-    all_builds_app_path(@query_params.merge(sort_column: column, sort_direction: direction))
-  end
-
-  def turbo_data
-    {turbo_frame: "all_builds"}
   end
 end
