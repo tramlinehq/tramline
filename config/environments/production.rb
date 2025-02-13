4660679007f06dff0f494589df24e8132cf56c2e
Akshay Gupta <kitallis@gmail.com>
2025-02-13 23:24:43 +0530

    Distributed locks around all Play Store API calls (#731)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use a different cache store in production.
-  config.cache_store = [:redis_cache_store, {url: ENV["DEFAULT_REDIS_URL"]}]
+  config.cache_store = REDIS_CONFIGURATION.cache
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
---
+++ lib/redis_configuration.rb
@@ -0,0 +1,47 @@
+class RedisConfiguration
+  CONNECT_TIMEOUT = 5
+
+  def base
+    @base ||= {
+      url: ENV["DEFAULT_REDIS_URL"],
+      driver: driver,
+      connect_timeout: CONNECT_TIMEOUT
+    }
+  end
+
+  # sidekiq does its own connection pooling
+  def sidekiq
+    @sidekiq ||= {
+      url: ENV["SIDEKIQ_REDIS_URL"],
+      driver: driver,
+      connect_timeout: CONNECT_TIMEOUT
+    }
+  end
+
+  def cache
+    @cache ||=
+      if ENV["DEFAULT_REDIS_URL"].present?
+        [:redis_cache_store,
+          {
+            url: ENV["DEFAULT_REDIS_URL"],
+            driver: driver,
+            connect_timeout: CONNECT_TIMEOUT,
+            pool: base_pool
+          }]
+      else
+        :memory_store
+      end
+  end
+
+  def base_pool
+    {
+      size: Integer(ENV["RAILS_MAX_THREADS"] || 5),
+      timeout: CONNECT_TIMEOUT
+    }
+  end
+
+  private
+
+  # this can eventually be changed to hiredis if required
+  def driver = :ruby
+end
