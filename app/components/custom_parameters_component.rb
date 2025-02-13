f86ba3fe361cdf48161489a253f433e1a0cece99
NileshGR <627281+nileshgr@users.noreply.github.com>
2025-02-13 15:01:19 +0530

    Use slots for heading and add button

--- app/components/custom_parameters_component.rb
+++ app/components/custom_parameters_component.rb
@@ -3,13 +3,14 @@
 class CustomParametersComponent < ViewComponent::Base
   attr_reader :form, :parameter_name_label, :parameter_value_label
 
-  def initialize(form, parameter_name_label: "Parameter Name", parameter_value_label: "Parameter Value", add_button: true, trash_button: true)
+  renders_one :add_button, ButtonComponent
+  renders_one :heading
+  renders_many :fields
+
+  def initialize(form, parameter_name_label: "Parameter Name", parameter_value_label: "Parameter Value", trash_button: true)
     @form = form
     @parameter_name_label = parameter_name_label
     @parameter_value_label = parameter_value_label
-    @add_button = add_button
     @trash_button = trash_button
   end
-
-  def add_button? = @add_button
 end
