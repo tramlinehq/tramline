29b4a7e0af0658ebf3f95ecad3927892b1a3b3f5
Sameer Kolhar <kolhar730@gmail.com>
2025-02-27 20:54:42 -0800

    add new constants.rb file in lib/seed to help store all constants needed for the seed script

---
+++ lib/seed/constants.rb
@@ -0,0 +1,17 @@
+# admin constants
+ADMIN_FULL_NAME = "Admin User"
+ADMIN_PREFERRED_NAME = "Admin"
+ADMIN_EMAIL = "admin@tramline.app"
+ADMIN_PASSWORD = "why aroma enclose startup"
+
+# owner constants
+OWNER_FULL_NAME = "Owner User"
+OWNER_PREFERRED_NAME = "Owner"
+OWNER_EMAIL = "owner@tramline.app"
+OWNER_PASSWORD = "why aroma enclose startup"
+
+# developer constants
+DEVELOPER_FULL_NAME = "Developer User"
+DEVELOPER_PREFERRED_NAME = "Developer"
+DEVELOPER_EMAIL = "developer@tramline.app"
+DEVELOPER_PASSWORD = "why aroma enclose startup"
