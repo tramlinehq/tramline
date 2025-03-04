1826d29a2f1284dd74b86969f4784ab175fe256a
Akshay Gupta <kitallis@gmail.com>
2025-03-04 14:32:59 +0530

    remove scoping for now

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,16 +2,6 @@ export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
     /**
-     * allowed scopes when committing
-     *
-     * e.g.
-     *
-     * - fix(feat): add jira integration
-     * - feat(chore): update the specs
-     *
-     */
-    "scope-enum": [2, "always", ["feat", "chore", "security"]],
-    /**
      * reduce header max length violation to a warning
      */
     "header-max-length": [1, "always", 72]
