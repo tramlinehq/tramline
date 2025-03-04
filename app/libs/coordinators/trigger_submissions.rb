b2e61e646a010ce5693c857822c48ce66c1efe1c
NileshGR <627281+nileshgr@users.noreply.github.com>
2025-03-04 15:07:03 +0530

    Simplify artifact handling

--- app/libs/coordinators/trigger_submissions.rb
+++ app/libs/coordinators/trigger_submissions.rb
@@ -12,7 +12,24 @@ class Coordinators::TriggerSubmissions
 
   def call
     return unless release_platform_run.on_track?
-    workflow_run.build.attach_artifact!
+
+    # Attempt to attach artifact only if platform is android
+    if release_platform_run.android?
+      begin
+        workflow_run.build.attach_artifact!
+      rescue Installations::Error => ex
+        if ex.reason == :artifact_not_found
+          # We can ignore the error if we find the build is found in store
+          workflow_run.build.mark_available_without_artifact
+        else
+          raise
+        end
+      end
+    elsif release_platform_run.ios?
+      # There are only two cases for platform - android/ios, keeping it elsif for clarity
+      # We don't handle uploads for ios (yet?)
+      workflow_run.build.mark_available_without_artifact
+    end
 
     if workflow_run.release_candidate? && release_platform_run.hotfix?
       Coordinators::StartProductionRelease.call(release_platform_run, workflow_run.build.id)
