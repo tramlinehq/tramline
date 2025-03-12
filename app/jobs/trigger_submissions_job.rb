e90bd83e9e2f47c5295d90951ff9deda59f64f9f
NileshGR <627281+nileshgr@users.noreply.github.com>
2025-03-12 10:47:11 +0530

    Retry after 300 seconds, use sidekiq perform_in to set next run time

--- app/jobs/trigger_submissions_job.rb
+++ app/jobs/trigger_submissions_job.rb
@@ -10,10 +10,10 @@ class TriggerSubmissionsJob < ApplicationJob
     if retry_count >= MAX_RETRIES
       workflow_run&.triggering_release&.fail!
     else
-      Rails.logger.debug { "Failed to fetch build artifact for workflow run #{workflow_run_id}, retrying in 30 seconds" }
-      TriggerSubmissionsJob
-        .set(wait_time: retry_count * 10.seconds)
-        .perform_async(workflow_run_id, retry_count + 1)
+      retry_count += 1
+      next_run_time = retry_count * 300.seconds
+      Rails.logger.debug { "Failed to fetch build artifact for workflow run #{workflow_run_id}, retrying in #{next_run_time} seconds" }
+      TriggerSubmissionsJob.perform_in(next_run_time, workflow_run_id, retry_count)
     end
   rescue => ex
     elog(ex, level: :warn)
