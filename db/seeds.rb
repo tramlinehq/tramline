25c312fa1f1fd26b751f13d94cf37cd71ad7ab8d
Sameer Kolhar <kolhar730@gmail.com>
2025-03-13 01:36:24 -0700

    update seeds.rb to ensure that demo seed script is run with right env var only

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
 # rubocop:disable Rails/Output
-Seed::DevStarter.call
+
+seed_mode = ENV["SEED_MODE"] || "dev"
+puts "Running seeds script in [#{seed_mode}]: "
+
+if seed_mode == "demo"
+  Seed::DemoStarter.call
+else
+  Seed::DevStarter.call
+end
+
 # rubocop:enable Rails/Output
