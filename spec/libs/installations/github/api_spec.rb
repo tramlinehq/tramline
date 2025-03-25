1a4d47169530371b011c531bde2eeb9e45bb8e8d
NileshGR <627281+nileshgr@users.noreply.github.com>
2025-03-25 17:28:35 +0530

    Add unique_number in test expectation

--- spec/libs/installations/github/api_spec.rb
+++ spec/libs/installations/github/api_spec.rb
@@ -66,7 +66,8 @@ describe Installations::Github::Api, type: :integration do
       expected = {
         ci_ref: 30433642,
         ci_link: "https://github.com/octo-org/octo-repo/actions/runs/30433642",
-        number: 562
+        number: 562,
+        unique_number: 30433642 # same as ci_ref
       }
       expect(result).to match(expected)
     end
