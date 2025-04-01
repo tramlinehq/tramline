b5234e9134528e20e73f243c2cd8bb94e3042510
NileshGR <627281+nileshgr@users.noreply.github.com>
2025-04-01 18:57:13 +0530

    Remove not-null constraint

--- db/migrate/20250331140348_add_automatic_rollout_columns.rb
+++ db/migrate/20250331140348_add_automatic_rollout_columns.rb
@@ -1,6 +1,6 @@
 class AddAutomaticRolloutColumns < ActiveRecord::Migration[7.2]
   def change
     add_column :store_rollouts, :automatic_rollout, :boolean, null: false, default: false
-    add_column :submission_configs, :automatic_rollout, :boolean, null: false, default: false
+    add_column :submission_configs, :automatic_rollout, :boolean, default: false
   end
 end
