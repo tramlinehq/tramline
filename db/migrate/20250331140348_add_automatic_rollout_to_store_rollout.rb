4051a5c6a35d29bfd40c4416cc1e3cd25d7adc99
NileshGR <627281+nileshgr@users.noreply.github.com>
2025-04-01 18:12:39 +0530

    Update migration for adding columns

--- db/migrate/20250331140348_add_automatic_rollout_to_store_rollout.rb
+++
@@ -1,5 +0,0 @@
-class AddAutomaticRolloutToStoreRollout < ActiveRecord::Migration[7.2]
-  def change
-    add_column :store_rollouts, :automatic_rollout, :boolean, null: false, default: false
-  end
-end
