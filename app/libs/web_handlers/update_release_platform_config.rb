3827b4d6bd19b77f284fa223acc0dfe753f98c8a
Akshay Gupta <kitallis@gmail.com>
2025-04-04 04:20:50 +0530

    reorder and normalize submission positions before saving

--- app/libs/web_handlers/update_release_platform_config.rb
+++ app/libs/web_handlers/update_release_platform_config.rb
@@ -20,24 +20,20 @@ class WebHandlers::UpdateReleasePlatformConfig
   def call
     self.processed_params = original_params
 
+    # @config.errors.merge!(service.errors) # Uncomment if form re-renders and needs errors
     ActiveRecord::Base.transaction do
-      handle_conditional_destruction
-      enrich_workflow_data
-      enrich_submission_data
-      reorder_submission_data
-      enrich_production_data
+      params =
+        processed_params
+          .then { handle_conditional_destruction(_1) }
+          .then { enrich_workflow_data(_1) }
+          .then { enrich_submission_data(_1) }
+          .then { enrich_production_data(_1) }
 
-      # @config.errors.merge!(service.errors) # Uncomment if form re-renders and needs errors
-      config.update!(processed_params)
+      config.update!(params)
     end
-
     errors.empty?
   rescue ActiveRecord::RecordInvalid => e
     copy_errors_from(e.record)
-    false
-  rescue => e
-    Rails.logger.error("UpdateReleasePlatformConfigService failed: #{e.message}\n#{e.backtrace.join("\n")}")
-    errors.add(:base, "An unexpected error occurred: #{e.message}")
     false
   end
 
@@ -46,14 +42,14 @@ class WebHandlers::UpdateReleasePlatformConfig
   attr_reader :original_params, :submission_types, :ci_actions, :release_platform
   attr_accessor :processed_params, :internal_submission_order_map, :beta_submission_order_map
 
-  def handle_conditional_destruction
+  def handle_conditional_destruction(processed_params)
     internal_enabled = original_params[:internal_release_enabled] == "true"
     beta_enabled = original_params[:beta_release_submissions_enabled] == "true"
     prod_enabled = original_params[:production_release_enabled] == "true"
 
     if !internal_enabled && processed_params[:internal_release_attributes].present?
       set_destroy!(processed_params[:internal_release_attributes])
-      set_destroy!(processed_params[:internal_workflow_attributes]) if processed_params[:internal_workflow_attributes]
+      set_destroy!(processed_params[:internal_workflow_attributes])
     end
 
     if !beta_enabled && processed_params[:beta_release_attributes].present?
@@ -66,31 +62,27 @@ class WebHandlers::UpdateReleasePlatformConfig
     if !prod_enabled && processed_params[:production_release_attributes].present?
       set_destroy!(processed_params[:production_release_attributes])
     end
+
+    processed_params
   end
 
-  def enrich_workflow_data
+  def enrich_workflow_data(processed_params)
     update_workflow_name(processed_params[:internal_workflow_attributes])
     update_workflow_name(processed_params[:release_candidate_workflow_attributes])
+    processed_params
   end
 
-  def enrich_submission_data
+  def enrich_submission_data(processed_params)
     update_submissions(processed_params[:internal_release_attributes])
     update_submissions(processed_params[:beta_release_attributes])
+
+    reorder_submissions(@internal_submissions_relation&.reload, processed_params[:internal_release_attributes])
+    reorder_submissions(@beta_submissions_relation&.reload, processed_params[:beta_release_attributes])
+
+    processed_params
   end
 
-  def reorder_submission_data
-    reorder_submissions(
-      @internal_submissions_relation&.reload,
-      processed_params&.dig(:internal_release_attributes, :submissions_attributes)
-    )
-
-    reorder_submissions(
-      @beta_submissions_relation&.reload,
-      processed_params&.dig(:beta_release_attributes, :submissions_attributes)
-    )
-  end
-
-  def enrich_production_data
+  def enrich_production_data(processed_params)
     prod_attrs = processed_params[:production_release_attributes]
 
     if release_platform.android? && prod_attrs.present?
@@ -103,19 +95,21 @@ class WebHandlers::UpdateReleasePlatformConfig
         submission_attrs[:finish_rollout_in_next_release] = false
       end
     end
+
+    processed_params
   end
 
   def update_submissions(release_attributes)
-    if release_attributes.present?
-      release_attributes[:submissions_attributes]&.each do |_, submission|
-        variant = submission_types[:variants].find { |v| v[:id] == submission[:integrable_id] }
-        submission[:integrable_type] = variant[:type]
+    return if release_attributes.blank?
 
-        ext_sub = find_submission(submission, variant)
-        if ext_sub.present? && submission[:submission_external_attributes].present?
-          submission[:submission_external_attributes][:name] = ext_sub[:name]
-          submission[:submission_external_attributes][:internal] = ext_sub[:is_internal]
-        end
+    release_attributes[:submissions_attributes]&.each do |_, submission|
+      variant = submission_types[:variants].find { |v| v[:id] == submission[:integrable_id] }
+      submission[:integrable_type] = variant[:type]
+
+      ext_sub = find_submission(submission, variant)
+      if ext_sub.present? && submission[:submission_external_attributes].present?
+        submission[:submission_external_attributes][:name] = ext_sub[:name]
+        submission[:submission_external_attributes][:internal] = ext_sub[:is_internal]
       end
     end
   end
@@ -124,6 +118,37 @@ class WebHandlers::UpdateReleasePlatformConfig
     if workflow_attributes&.dig(:identifier).present?
       workflow_attributes[:name] = find_workflow_name(workflow_attributes[:identifier])
     end
+  end
+
+  def reorder_submissions(existing_submissions_relation, release_attributes)
+    return if existing_submissions_relation.blank? || release_attributes.blank?
+
+    # create a copy of the submissions attributes to allow sorting by number
+    sorted_submissions =
+      release_attributes[:submissions_attributes]
+        .to_h
+        .reject { |_, attrs| attrs["_destroy"] == "1" }
+        .sort_by { |_, attrs| attrs["number"].to_i }
+
+    # set all existing submissions to temporary negative numbers to avoid conflicts
+    existing_submissions = existing_submissions_relation.index_by(&:id)
+    existing_submissions.each do |_, submission|
+      submission.update!(number: -submission.number)
+    end
+
+    sorted_submissions.each_with_index do |(id, submission_attrs), index|
+      new_number = index + 1
+
+      # update the original release attributes
+      release_attributes[:submissions_attributes][id]["number"] = new_number.to_s
+
+      # update the existing submissions in the DB
+      if submission_attrs["id"].present? && (existing_submission = existing_submissions[submission_attrs["id"].to_i])
+        existing_submission.update!(number: new_number)
+      end
+    end
+
+    release_attributes
   end
 
   def find_workflow_name(identifier)
@@ -142,32 +167,6 @@ class WebHandlers::UpdateReleasePlatformConfig
 
   def set_destroy!(param)
     param[:_destroy] = "1" if param.present?
-  end
-
-  def reorder_submissions(existing_submissions_relation, submissions_attributes)
-    return if existing_submissions_relation.blank? || submissions_attributes.blank?
-    existing_submissions = existing_submissions_relation.index_by(&:id)
-
-    sorted_submissions =
-      submissions_attributes
-        .to_h
-        .reject { |_, attrs| attrs["_destroy"] == "1" }
-        .sort_by { |_, attrs| attrs["number"].to_i }
-
-    # set all existing submissions to temporary negative numbers to avoid conflicts
-    existing_submissions.each do |_, submission|
-      submission.update(number: -submission.number)
-    end
-
-    # update both params and DB with new sequential numbers
-    sorted_submissions.each_with_index do |(_, submission_attrs), index|
-      new_number = index + 1
-      submission_attrs["number"] = new_number.to_s
-
-      if submission_attrs["id"].present? && (existing_submission = existing_submissions[submission_attrs["id"].to_i])
-        existing_submission.update(:number, new_number)
-      end
-    end
   end
 
   def copy_errors_from(record)
