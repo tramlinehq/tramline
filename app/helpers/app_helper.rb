0c22b71cd5b9224db49a7cd15fd5c0631c6bbffd
NileshGR <627281+nileshgr@users.noreply.github.com>
2025-04-30 16:04:59 +0530

    Move channel pattern to helper and show release specific pattern in notification settings

---
+++ app/helpers/app_helper.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module AppHelper
+  def release_specific_channel_pattern(app)
+    platform = app.cross_platform? ? "" : "-#{app.platform}"
+    channel_pattern = "release-#{app.name}#{platform}".downcase.gsub(/\W/, "-")
+    "##{channel_pattern}-{version}"
+  end
+end
