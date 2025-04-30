8d19777160d2b12124353cbf432cebe86ead335c
NileshGR <627281+nileshgr@users.noreply.github.com>
2025-04-30 17:51:04 +0530

    Remove # from channel pattern

--- app/helpers/app_helper.rb
+++ app/helpers/app_helper.rb
@@ -4,6 +4,6 @@ module AppHelper
   def release_specific_channel_pattern(app)
     platform = app.cross_platform? ? "" : "-#{app.platform}"
     channel_pattern = "release-#{app.name}#{platform}".downcase.gsub(/\W/, "-")
-    "##{channel_pattern}-{version}"
+    "#{channel_pattern}-{version}"
   end
 end
