09b2852f97f3bafde262877f65b5d4f32ea285c4
NileshGR <627281+nileshgr@users.noreply.github.com>
2025-05-01 20:29:22 +0530

    feat: Release specific notification channels (#794)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_19_082238) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_25_095631) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "pg_trgm"
@@ -725,6 +725,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_19_082238) do
     t.string "slug"
     t.boolean "is_v2", default: false
     t.uuid "approval_overridden_by_id"
+    t.jsonb "notification_channel"
     t.index ["approval_overridden_by_id"], name: "index_releases_on_approval_overridden_by_id"
     t.index ["slug"], name: "index_releases_on_slug", unique: true
     t.index ["train_id"], name: "index_releases_on_train_id"
@@ -952,6 +953,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_19_082238) do
     t.string "version_bump_file_paths", default: [], array: true
     t.string "version_bump_branch_prefix"
     t.string "continuous_backmerge_branch_prefix"
+    t.boolean "notifications_release_specific_channel_enabled", default: false
     t.index ["app_id"], name: "index_trains_on_app_id"
   end
 
