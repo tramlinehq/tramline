3b7a61cfa25cd8fbc29558170e74e5a14bc7dc8e
Akshay Gupta <kitallis@gmail.com>
2025-05-29 17:56:19 +0530

    Revert "revert me: fail both"
    This reverts commit c8a6e3c9ba0f8aafa0e5ab1c6d1a67af700e3d59.

--- db/migrate/20250529121033_add_version_bumping_strategc.rb
+++
@@ -1,5 +0,0 @@
-class AddVersionBumpingStrategc < ActiveRecord::Migration[7.2]
-  def change
-    add_column :trains, :delete_me_to, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_29_121033) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_29_114812) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "pg_trgm"
@@ -965,7 +965,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_29_121033) do
     t.boolean "tag_store_releases_vcs_release", default: false
     t.boolean "notifications_release_specific_channel_enabled", default: false
     t.string "delete_me"
-    t.string "delete_me_to"
     t.index ["app_id"], name: "index_trains_on_app_id"
   end
 
