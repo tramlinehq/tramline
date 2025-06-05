a3e9399e69de23e260c6ba1f473bbee7ce56b618
NileshGR <627281+nileshgr@users.noreply.github.com>
2025-06-05 19:13:52 +0530

    Send empty collection when we do not want changes_since_last_run to be consumed

--- app/models/pre_prod_release.rb
+++ app/models/pre_prod_release.rb
@@ -162,21 +162,16 @@ class PreProdRelease < ApplicationRecord
   end
 
   def notification_params
-    params = release_platform_run.notification_params.merge(
+    release_platform_run.notification_params.merge(
       commit_sha: commit.short_sha,
       commit_url: commit.url,
       build_number: build.build_number,
       release_version: release.release_version,
       submission_channels: store_submissions.map { |s| "#{s.provider.display} - #{s.submission_channel.name}" }.join(", "),
       submissions: store_submissions,
-      changes_since_last_release:
+      changes_since_last_release:,
+      changes_since_last_run: previous_successful.present? ? changes_since_last_run : []
     )
-
-    if previous_successful.present?
-      params[:changes_since_last_run] = changes_since_last_run
-    end
-
-    params
   end
 
   def latest_events(n = nil)
