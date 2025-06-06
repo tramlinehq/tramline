c0eb6f788bb24c10854682e70a208535ba3a25c9
NileshGR <627281+nileshgr@users.noreply.github.com>
2025-06-06 12:46:38 +0530

    Remove the splitting logic from template

--- app/libs/notifiers/slack/renderers/changelog.rb
+++ app/libs/notifiers/slack/renderers/changelog.rb
@@ -12,16 +12,6 @@ module Notifiers
       def render_footer
         {blocks: []}.to_json
       end
-
-      def changes_in_message(changes, &)
-        changes[0, changes_limit].each_with_index do |change, i|
-          yield change, i
-        end
-      end
-
-      def changes_limit
-        NotificationSetting::CHANGELOG_PER_MESSAGE_LIMIT
-      end
     end
   end
 end
--- app/libs/notifiers/slack/renderers/rc_finished.rb
+++ app/libs/notifiers/slack/renderers/rc_finished.rb
@@ -12,20 +12,6 @@ module Notifiers
           ":white_check_mark: Submitted to *<#{submission.display}>"
         end
       end
-
-      def changes_in_main_message(changes, &)
-        changes[0, changes_limit].each_with_index do |change, i|
-          yield change, i
-        end
-      end
-
-      def changes_spillover?(changes)
-        changes.size > changes_limit
-      end
-
-      def changes_limit
-        NotificationSetting::CHANGELOG_PER_MESSAGE_LIMIT
-      end
     end
   end
 end
