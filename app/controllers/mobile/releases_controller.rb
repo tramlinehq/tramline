a98fbfcb9d00c565e1d46492a972c9516e7d9705
Akshay Gupta <kitallis@gmail.com>
2025-07-03 17:09:17 -0400

    fix suggestions from coderabbit

--- app/controllers/mobile/releases_controller.rb
+++ app/controllers/mobile/releases_controller.rb
@@ -11,8 +11,9 @@ module Mobile
     def show
       @release = current_organization.releases.friendly.find(params[:id])
       @release_presenter = ReleasePresenter.new(@release, view_context)
-      @inflight_rollouts = @release.release_platform_runs.filter_map(&:inflight_store_rollout).group_by(&:platform)
-      @active_rollouts = @release.release_platform_runs.filter_map(&:active_store_rollout).group_by(&:platform)
+      platform_runs = @release.release_platform_runs
+      @inflight_rollouts = platform_runs.filter_map(&:inflight_store_rollout).group_by(&:platform)
+      @active_rollouts = platform_runs.filter_map(&:active_store_rollout).group_by(&:platform)
     rescue ActiveRecord::RecordNotFound
       redirect_to mobile_releases_path, alert: t(".not_found")
     end
