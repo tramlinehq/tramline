55e68af32397b9c20885dba445ceb19da2710c57
Akshay Gupta <kitallis@gmail.com>
2025-07-08 20:49:12 +0530

    refactor

--- app/models/concerns/sanitizable.rb
+++ app/models/concerns/sanitizable.rb
@@ -1,15 +1,18 @@
 module Sanitizable
   extend ActiveSupport::Concern
 
+  COMMIT_FILTER_PATTERNS = /\AMerge|\ACo-authored-by|\A---------/.freeze
+  EMOJI_PATTERN = /\p{Emoji_Presentation}\s*/.freeze
+
   private
 
   def sanitize_commit_messages(array_of_commit_messages, compact_messages: true)
     array_of_commit_messages
       .map { |str| str&.strip }
       .flat_map { |line| compact_messages ? line.split("\n").first : line.split("\n") }
-      .map { |line| line.gsub(/\p{Emoji_Presentation}\s*/, "") }
+      .map { |line| line.gsub(EMOJI_PATTERN, "") }
       .map { |line| line.gsub('"', "\\\"") }
-      .reject { |line| line =~ /\AMerge|\ACo-authored-by|\A---------/ }
+      .reject { |line| line =~ COMMIT_FILTER_PATTERNS }
       .compact_blank
       .uniq
   end
