e3f258c3805dfc974cb074ccf04d914f6d4ce398
Akshay Gupta <kitallis@gmail.com>
2025-08-06 15:10:02 +0530

    remove super_admin

--- app/models/accounts/user.rb
+++ app/models/accounts/user.rb
@@ -48,11 +48,7 @@ class Accounts::User < ApplicationRecord
 
   accepts_nested_attributes_for :organizations
   accepts_nested_attributes_for :memberships, allow_destroy: false
-
-  def super_admin?
-    Flipper.enabled?(:super_admin, self)
-  end
-
+  
   def show_crashlytics_stats?
     Flipper.enabled?(:show_crashlytics_stats, self)
   end
