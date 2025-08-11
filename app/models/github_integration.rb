ec6036628fd2370222f2bb39d473420a30b361b9
Akshay Gupta <kitallis@gmail.com>
2025-08-11 19:55:49 +0530

    chore: only support Deploy-Action for GitHub (#862)
    ref: https://github.com/tramlinehq/deploy-action

--- app/models/github_integration.rb
+++ app/models/github_integration.rb
@@ -241,8 +241,8 @@ class GithubIntegration < ApplicationRecord
     end
   end
 
-  def trigger_workflow_run!(ci_cd_channel, branch_name, inputs, commit_hash = nil, deploy_action_enabled = false)
-    installation.run_workflow!(code_repository_name, ci_cd_channel, branch_name, inputs, commit_hash, deploy_action_enabled)
+  def trigger_workflow_run!(ci_cd_channel, branch_name, inputs, commit_hash = nil)
+    installation.run_workflow!(code_repository_name, ci_cd_channel, branch_name, inputs, commit_hash)
   end
 
   def cancel_workflow_run!(ci_ref)
