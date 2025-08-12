b507ccd1bea78b2f841a9def284b80da0055a91b
Animesh-Ghosh <animesh.ghosh@tuta.io>
2025-08-12 23:30:02 +0530

    feat: Compute scheduled release version based on repeat duration
    Update component spec to follow table-driven testing style.
    Delegate Releast#repeat_duration to the release train.

--- spec/components/scheduled_train_component_spec.rb
+++ spec/components/scheduled_train_component_spec.rb
@@ -5,19 +5,16 @@ RSpec.describe ScheduledTrainComponent, type: :component do
 
   describe "#next_next_version" do
     context "when using SemVer strategy" do
-      context "when there is no ongoing release" do
-        let(:train) { create(:train, app:, version_seeded_with: "1.2.3", versioning_strategy: :semver) }
-        let(:component) { described_class.new(train) }
+      let(:train) { create(:train, app:, version_seeded_with: "1.2.3", versioning_strategy: :semver) }
+      let(:component) { described_class.new(train) }
 
+      context "when there is no ongoing release" do
         it "returns the train's next-to-next version" do
           expect(component.next_next_version).to eq("1.4.0")
         end
       end
 
       context "when there is an ongoing release" do
-        let(:train) { create(:train, app:, version_seeded_with: "1.2.3", versioning_strategy: :semver) }
-        let(:component) { described_class.new(train) }
-
         before do
           create(:release, :on_track, train:, original_release_version: "1.3.0")
         end
@@ -29,31 +26,48 @@ RSpec.describe ScheduledTrainComponent, type: :component do
     end
 
     context "when using CalVer strategy" do
-      before do
-        travel_to(Time.new(2025, 5, 23, 12, 0, 0, "+00:00"))
-      end
-
       context "when there is no ongoing release" do
-        let(:train) { create(:train, app:, version_seeded_with: "2025.05.22", versioning_strategy: :calver) }
-        let(:component) { described_class.new(train) }
-
-        it "returns the train's future version (next day) using future_version" do
-          expect(component.next_version).to eq("2025.05.23")  # Current date
-          expect(component.next_next_version).to eq("2025.05.24")  # Next day
+        [
+          [Time.new(2025, 5, 23, 22, 0, 0, "+00:00"), 1.day, "2025.05.24"],
+          [Time.new(2025, 5, 31, 0, 0, 0, "+00:00"), 1.day, "2025.06.01"],
+          [Time.new(2025, 12, 31, 0, 0, 0, "+00:00"), 1.day, "2026.01.01"],
+          [Time.new(2025, 5, 23, 22, 0, 0, "+00:00"), 2.days, "2025.05.25"],
+          [Time.new(2025, 5, 23, 22, 0, 0, "+00:00"), 1.week, "2025.05.30"]
+        ].each do |test_time, repeat_duration, expected_result|
+          it "returns the train's next-to-next version scheduled #{repeat_duration.inspect} after #{test_time.strftime("%Y.%d.%m")}" do
+            train = create(:train, :with_schedule,
+              app:,
+              version_seeded_with: "2025.01.01",
+              versioning_strategy: :calver,
+              repeat_duration: repeat_duration)
+            component = described_class.new train
+            travel_to(test_time) do
+              expect(component.next_next_version).to eq(expected_result)
+            end
+          end
         end
       end
 
       context "when there is an ongoing release" do
-        let(:train) { create(:train, app:, version_seeded_with: "2025.05.22", versioning_strategy: :calver) }
-        let(:component) { described_class.new(train) }
-
-        before do
-          create(:release, :on_track, train: train, original_release_version: "2025.05.23")
-        end
-
-        it "returns the ongoing release's future version (next day) using future_version" do
-          expect(component.next_version).to eq("2025.05.23")  # Current date
-          expect(component.next_next_version).to eq("2025.05.24")  # Next day
+        [
+          [Time.new(2025, 5, 23, 22, 0, 0, "+00:00"), 1.day, "2025.05.24"],
+          [Time.new(2025, 5, 31, 0, 0, 0, "+00:00"), 1.day, "2025.06.01"],
+          [Time.new(2025, 12, 31, 0, 0, 0, "+00:00"), 1.day, "2026.01.01"],
+          [Time.new(2025, 5, 23, 22, 0, 0, "+00:00"), 2.days, "2025.05.25"],
+          [Time.new(2025, 5, 23, 22, 0, 0, "+00:00"), 1.week, "2025.05.30"]
+        ].each do |test_time, repeat_duration, expected_result|
+          it "returns the release's next-to-next version scheduled #{repeat_duration.inspect} after #{test_time.strftime("%Y.%d.%m")}" do
+            train = create(:train, :with_schedule,
+              app:,
+              version_seeded_with: "2025.01.01",
+              versioning_strategy: :calver,
+              repeat_duration: repeat_duration)
+            create(:release, :on_track, train: train, original_release_version: "2025.01.31")
+            component = described_class.new train
+            travel_to(test_time) do
+              expect(component.next_next_version).to eq(expected_result)
+            end
+          end
         end
       end
     end
