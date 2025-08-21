f3b6a4a7323a6ac05da44785c5dad4c588028479
Akshay Gupta <kitallis@gmail.com>
2025-08-21 21:53:03 +0530

    Add india gcs storage

--- db/migrate/20250818012307_create_accounts_custom_storages.rb
+++ db/migrate/20250818012307_create_accounts_custom_storages.rb
@@ -1,12 +1,5 @@
-class CreateAccountsCustomStorages < ActiveRecord::Migration[7.0]
+class AddCustomStorageServiceKeyToOrgs < ActiveRecord::Migration[7.0]
   def change
-    create_table :custom_storages do |t|
-      t.references :organization, null: false, foreign_key: {to_table: :organizations}, type: :uuid, index: {unique: true}
-      t.string :bucket, null: false
-      t.string :project_id, null: false
-      t.jsonb :credentials, null: false
-
-      t.timestamps
-    end
+    add_column :organizations, :custom_storage_service_key, :string
   end
 end
