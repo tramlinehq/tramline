9b96fda04ced3dcb1944601e979e8c5a19cb49c4
Akshay Gupta <kitallis@gmail.com>
2025-09-12 23:29:11 +0530

    feat: allow disabling all back-merges

--- spec/libs/coordinators/finalize_release/parallel_branches_spec.rb
+++ spec/libs/coordinators/finalize_release/parallel_branches_spec.rb
@@ -3,5 +3,6 @@ require "rails_helper"
 describe Coordinators::FinalizeRelease::ParallelBranches do
   describe ".call" do
     it_behaves_like "end of release tagging"
+    it_behaves_like "end of release pull request creation for parallel branches"
   end
 end
