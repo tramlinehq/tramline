95c8952fb0f8ac1478bc090f53aca126a901b933
Akshay Gupta <kitallis@gmail.com>
2025-11-14 18:44:42 +0530

    add vcs logo in vcs integration configuration

--- db/migrate/20251114093633_migrate_app_variant_from_app_config_to_app.rb
+++ db/migrate/20251114093633_migrate_app_variant_from_app_config_to_app.rb
@@ -3,10 +3,10 @@ class MigrateAppVariantFromAppConfigToApp < ActiveRecord::Migration[7.2]
     safety_assured do
       # Add app_id column to app_variants (keeping app_config_id for now)
       add_reference :app_variants, :app, null: true, foreign_key: true, type: :uuid
-      
+
       # Make app_config_id nullable (in preparation for removal)
       change_column_null :app_variants, :app_config_id, true
-      
+
       # Add unique index for new app_id relationship (matching the old pattern)
       add_index :app_variants, [:bundle_identifier, :app_id], unique: true, name: "index_app_variants_on_bundle_identifier_and_app_id"
     end
