dc5d7d1bccb6d5136813addc00d2eba5941e89f1
Animesh-Ghosh <animesh.ghosh@tuta.io>
2025-11-26 00:21:21 +0530

    feat: Add "Start release" button on Release index page on mobile view
    TODO: Improve mobile responsiveness for the new `PrepareReleaseComponent`.

--- app/components/prepare_release_component.rb
+++ app/components/prepare_release_component.rb
@@ -1,25 +1,17 @@
-class ReleaseListComponent < BaseComponent
+class PrepareReleaseComponent < BaseComponent
   include Memery
 
-  def initialize(train:)
+  REVEAL_HIDE_ACTION = "reveal#hide"
+  REVEAL_SHOW_ACTION = "reveal#show"
+
+  def initialize(train:, label: "Prepare new release", size: :xxs)
     @train = train
-    @ongoing_release = train.ongoing_release
-    @hotfix_release = train.hotfix_release
-    @upcoming_release = train.upcoming_release
+    @label = label
+    @size = size
   end
 
-  attr_reader :train, :ongoing_release, :hotfix_release, :upcoming_release
-  delegate :app, :hotfix_from, :previous_releases, :last_finished_release, to: :train
-
-  # we don't check for train.releases.none?
-  # because the constituent releases that are loaded on the page are already memoized, so we avoid a query
-  def no_releases?
-    previous_releases.empty? && ongoing_release.nil? && upcoming_release.nil? && hotfix_release.nil? && last_finished_release.nil?
-  end
-
-  memoize def devops_report
-    DevopsReportPresenter.new(train.devops_report)
-  end
+  attr_reader :train, :label, :size
+  delegate :app, to: :train
 
   def release_startable?
     app.ready? && release_options.present?
@@ -123,43 +115,6 @@ class ReleaseListComponent < BaseComponent
           "."
         ]
       )
-    end
-  end
-
-  def ios_enabled?
-    app.cross_platform? || app.ios?
-  end
-
-  def no_release_empty_state
-    unless app.ready?
-      return {
-        title: "App is not ready",
-        text: "There are required integrations that need to be configured before you can start creating releases.",
-        content: render(ButtonComponent.new(scheme: :light, type: :link, label: "Configure Integration", options: app_integrations_path(app), size: :xxs, authz: false))
-      }
-    end
-
-    if train.automatic?
-      if train.activatable?
-        {
-          title: "Activate the train",
-          text: "Once you've activated, we will automatically start running your scheduled releases."
-        }
-      else
-        {
-          title: "Upcoming release",
-          text: "Your first scheduled release will automatically kick-off at #{train.kickoff_at.to_fs(:short)}. You can also manually run a new release by clicking the prepare button."
-        }
-      end
-    else
-      platform = train.release_platforms.first.platform
-      text = "You can now start creating new releases. We have added some default submissions settings for you. This involves picking the right workflows and configuring the right channels for build distribution. Please review these before starting a release."
-      button_link = edit_app_train_platform_submission_config_path(app, train, platform)
-      {
-        title: "Create your very first release",
-        text:,
-        content: render(ButtonComponent.new(scheme: :light, type: :link, label: "Review submission settings", options: button_link, size: :xxs, authz: false))
-      }
     end
   end
 
