9d22b1bb06e40ebabc8e3c3c0824b2533d4a4176
Animesh-Ghosh <animesh.ghosh@tuta.io>
2026-01-12 00:23:21 +0530

    feat: Add a few ViewComponents
    List all steps, highlight current step and completed steps and render
    navigation links for steps.
    Demonstrate validation UX and completion UX.

--- app/controllers/onboarding_controller.rb
+++ app/controllers/onboarding_controller.rb
@@ -11,11 +11,25 @@ class OnboardingController < SignedInApplicationController
   end
 
   def update
+    handle_step_2 and return if step == :step_2
+
     @onboarding_state.update(onboarding_state_params)
+    flash[:notice] = "Onboarding completed!" if step == steps.last
     render_wizard @onboarding_state
   end
 
   private
+
+  def handle_step_2
+    if onboarding_state_params[:field_2].present?
+      @onboarding_state.update(onboarding_state_params)
+      render_wizard @onboarding_state
+    else
+      @onboarding_state.errors.add(:field_2, "can't be blank")
+      # redirect or render the same step with error, subsequently render the error in the view in the apt place
+      redirect_back fallback_location: wizard_url(step), flash: {error: "#{@onboarding_state.errors.full_messages.to_sentence}."}
+    end
+  end
 
   def set_onboarding_state
     @onboarding_state = OnboardingState.find_or_create_by!(app: @app)
