39b74b766194733e2dec1a829728180b5b73722c
Animesh-Ghosh <animesh.ghosh@tuta.io>
2026-01-28 09:03:29 +0530

    fix: Allow comparison between a partial and proper Semver
    `RefreshReportsJob` would fail if a Train's releases switched from
    partial Semver to proper Semver when comparing the recently finished
    release with previous ones.

--- spec/libs/versioning_strategies/semverish_spec.rb
+++ spec/libs/versioning_strategies/semverish_spec.rb
@@ -57,11 +57,13 @@ describe VersioningStrategies::Semverish do
       expect(v1 == v3).to be(false)
     end
 
-    it "does not compare a partial and a proper semver" do
+    it "compares a partial and a proper semver" do
       bigger = described_class.new("1.2.1")
       smaller = described_class.new("1.2")
 
-      expect { bigger > smaller }.to raise_error(ArgumentError)
+      expect do
+        expect(bigger > smaller).to be(true)
+      end.not_to raise_error
     end
 
     it "determines sort order" do
@@ -78,6 +80,21 @@ describe VersioningStrategies::Semverish do
 
       expect([v1, v2, v3, v4].sort).to eq([v3, v1, v2, v4])
       expect([pv1, pv2, pv3, pv4, pv5].sort).to eq([pv5, pv3, pv4, pv1, pv2])
+    end
+
+    it "determines sort order with mixed partial and proper semverish" do
+      v1 = described_class.new("1.2.1")
+      v2 = described_class.new("1.2.3")
+      v3 = described_class.new("1.1.3")
+      v4 = described_class.new("2.1.3")
+
+      pv1 = described_class.new("2.1")
+      pv2 = described_class.new("2.1")
+      pv3 = described_class.new("1.2")
+      pv4 = described_class.new("1.3")
+      pv5 = described_class.new("0.1")
+
+      expect([v1, v2, v3, v4, pv1, pv2, pv3, pv4, pv5].sort).to eq([pv5, v3, pv3, v1, v2, pv4, pv1, pv2, v4])
     end
   end
 
