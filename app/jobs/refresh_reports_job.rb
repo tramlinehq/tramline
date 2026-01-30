4c8f567f7e49c1574a6d9adb3a56c9f794c95d68
Animesh-Ghosh <animesh.ghosh@tuta.io>
2026-01-30 09:29:52 +0530

    chore: Remove TODO

--- app/jobs/refresh_reports_job.rb
+++ app/jobs/refresh_reports_job.rb
@@ -1,5 +1,4 @@
 class RefreshReportsJob < ApplicationJob
-  # TODO: add a spec for partial -> proper semver comparison
   def perform(release_id)
     release = Release.find(release_id)
     train = release.train
